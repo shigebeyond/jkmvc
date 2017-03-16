87802670f9193aae4f4b2bc994b5e8e051b14874
shijianhang <772910474.com>
2017-03-16 15:29:19 +0800

    OrmRelated提供original()方法以便接收原始数据

--- src/main/kotlin/com/jkmvc/orm/IOrmRelated.kt
+++ src/main/kotlin/com/jkmvc/orm/IOrmRelated.kt
@@ -12,6 +12,14 @@ package com.jkmvc.orm
 interface IOrmRelated : IOrmPersistent
 {
 	/**
+	 * 设置原始的字段值
+	 *
+	 * @param array data
+	 * @return Orm|array
+	 */
+	public fun original(data: Map<String, Any?>): IOrm;
+
+	/**
 	 * 获得关联对象
 	 *
 	 * @param string name 关联对象名
@@ -20,5 +28,5 @@ interface IOrmRelated : IOrmPersistent
 	 * 													如 array("name", "age", "birt" => "birthday"), 其中 name 与 age 字段不带别名, 而 birthday 字段带别名 birt
 	 * @return Orm
 	 */
-	public fun related(name:String, newed:Boolean = false, vararg columns:String): IOrm;
+	public fun related(name:String, newed:Boolean = false, vararg columns:String): Any?;
 }
--- src/main/kotlin/com/jkmvc/orm/OrmRelated.kt
+++ src/main/kotlin/com/jkmvc/orm/OrmRelated.kt
@@ -11,7 +11,7 @@ import java.util.*
  * @date 2016-10-10 上午12:52:34
  *
  */
-open abstract class OrmRelated(data: MutableMap<String, Any?> = LinkedHashMap<String, Any?>()) : OrmPersistent(data) {
+open abstract class OrmRelated: OrmPersistent() {
     /**
      * 设置对象字段值
      *
@@ -19,9 +19,6 @@ open abstract class OrmRelated(data: MutableMap<String, Any?> = LinkedHashMap<St
      * @param  mixed  value  字段值
      */
     public override operator fun set(column: String, value: Any?) {
-        if (!hasColumn(column))
-            throw OrmException("类 class 没有字段 column");
-
         // 设置关联对象
         val relation = metadata.getRelation(column);
         if (relation != null) {
@@ -45,33 +42,30 @@ open abstract class OrmRelated(data: MutableMap<String, Any?> = LinkedHashMap<St
     public override operator fun <T> get(name: String, defaultValue: Any?): T {
         // 获得关联对象
         if (metadata.hasRelation(name))
-            return related(name);
+            return related(name) as T;
 
         return super.get(name, defaultValue);
     }
 
     /**
-     * 获得/设置原始的字段值
+     * 设置原始的字段值
      *
      * @param array data
      * @return Orm|array
      */
-    public fun original(data: Map<String, Any?>): Orm {
-        for ((column, value) in data) {
+    public override fun original(orgn: Map<String, Any?>): IOrm {
+        for ((column, value) in orgn) {
             // 关联查询时，会设置关联表字段的列别名（列别名 = 表别名 : 列名），可以据此来设置关联对象的字段值
-            if (!column.contains(":")) // 自身字段
-            {
-                this.data[column] = value;
-            }
-
-            if (value !== null) // 关联对象字段: 不处理null的值, 因为left join查询时, 关联对象可能没有匹配的行
-            {
+            if (!column.contains(":")){ // 自身字段
+                data[column] = value;
+            } else if (value !== null) {// 关联对象字段: 不处理null的值, 因为left join查询时, 关联对象可能没有匹配的行
                 val (name, column) = column.split(":");
-                val obj = this.related(name, true); // 创建关联对象
+                val obj:Orm = related(name, true) as Orm; // 创建关联对象
                 obj.data[column] = value;
             }
         }
 
+        loaded = true;
         return this;
     }
 
@@ -84,10 +78,10 @@ open abstract class OrmRelated(data: MutableMap<String, Any?> = LinkedHashMap<St
      * 													如 array("name", "age", "birt" => "birthday"), 其中 name 与 age 字段不带别名, 而 birthday 字段带别名 birt
      * @return Orm
      */
-    public override fun related(name: String, newed: Boolean, vararg columns: String): Orm {
+    public override fun related(name: String, newed: Boolean, vararg columns: String): Any? {
         // 已缓存
-        if (this.data.contains(name))
-            return this.data[name] as Orm;
+        if (data.contains(name))
+            return data[name] as Orm;
 
         // 获得关联关系
         val relation: MetaRelation = metadata.getRelation(name)!!;
@@ -95,23 +89,23 @@ open abstract class OrmRelated(data: MutableMap<String, Any?> = LinkedHashMap<St
         // 创建新对象
         if (newed) {
             val item = relation.model.java.newInstance() as Orm;
-            this.data[name] = item;
+            data[name] = item;
             return item;
         }
 
         // 根据关联关系来构建查询
-        var obj: Orm;
+        var result: Any?;
         when (relation.type) {
             RelationType.BELONGS_TO -> // belongsto: 查主表
-                obj = this.queryMaster(relation.metadata, relation.foreignKey).select(columns).find();
+                result = queryMaster(relation.metadata, relation.foreignKey).select(columns).find<IOrm>();
             RelationType.HAS_ONE -> // hasxxx: 查从表
-                obj = this.querySlave(relation.metadata, relation.foreignKey).select(columns).find();
+                result = querySlave(relation.metadata, relation.foreignKey).select(columns).find<IOrm>();
             else -> // hasxxx: 查从表
-                obj = this.querySlave(relation.metadata, relation.foreignKey).select(columns).findAll();
+                result = querySlave(relation.metadata, relation.foreignKey).select(columns).findAll<IOrm>();
         }
 
-        this.data[name] = obj;
-        return obj;
+        data[name] = result;
+        return result;
     }
 
     /**
@@ -121,8 +115,8 @@ open abstract class OrmRelated(data: MutableMap<String, Any?> = LinkedHashMap<St
      * @param string foreignKey 外键
      * @return OrmQueryBuilder
      */
-    protected fun querySlave(model: MetaData, foreignKey: String): OrmQueryBuilder {
-        return model.queryBuilder().where(foreignKey, this.pk()) as OrmQueryBuilder; // 从表.外键 = 主表.主键
+    protected fun querySlave(model: IMetaData, foreignKey: String): OrmQueryBuilder {
+        return model.queryBuilder().where(foreignKey, pk()) as OrmQueryBuilder; // 从表.外键 = 主表.主键
     }
 
     /**
@@ -132,7 +126,7 @@ open abstract class OrmRelated(data: MutableMap<String, Any?> = LinkedHashMap<St
      * @param string foreignKey 外键
      * @return OrmQueryBuilder
      */
-    protected fun queryMaster(model: MetaData, foreignKey: String): OrmQueryBuilder {
+    protected fun queryMaster(model: IMetaData, foreignKey: String): OrmQueryBuilder {
         return model.queryBuilder().where(metadata.primaryKey, foreignKey) as OrmQueryBuilder; // 主表.主键 = 从表.外键
     }
 }
