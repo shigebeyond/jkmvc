88ca9e0ec51eb1c475e85207d53d744c370ca2be
shijianhang <772910474.com>
2017-03-16 17:48:07 +0800

    扩展KClass<out IOrm>类来添加modelName与modelMetaData属性

--- src/main/kotlin/com/jkmvc/orm/_Orm.kt
+++ src/main/kotlin/com/jkmvc/orm/_Orm.kt
@@ -1,5 +1,25 @@
 package com.jkmvc.orm
 
+import kotlin.reflect.KClass
+import kotlin.reflect.companionObjectInstance
+
+/**
+ * 检查数据是否存在于db
+ */
 public fun Orm?.isLoaded(): Boolean {
     return this != null && this.loaded;
 }
+
+/**
+ * 根据模型类来获得模型名
+ *   假定model类名, 都是以"Model"作为后缀
+ */
+public val KClass<out IOrm>.modelName:String
+    get() = this.simpleName!!.removeSuffix("Model").toLowerCase();
+
+/**
+ * 根据模型类来获得模型元数据
+ *   随对象就是元数据
+ */
+public val KClass<out IOrm>.modelMetaData:IMetaData
+    get() = companionObjectInstance as IMetaData
