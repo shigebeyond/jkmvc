b7dd2dc6c88fbd9fe488da5d96699f4baa854a98
shijianhang <3076679995@qq.com>
2017-03-17 01:26:06 +0800

    简化关联配置后的单元测试

--- src/test/kotlin/com/jkmvc/tests/OrmTests.kt
+++ src/test/kotlin/com/jkmvc/tests/OrmTests.kt
@@ -15,7 +15,7 @@ class UserModel(id:Int? = null): Orm(id) {
     public var age:Int by m.property<Int>();
 
     // 关联地址：一个用户有一个地址
-    public var address:AddressModel by m.property<AddressModel>();
+    public var address:AddressModel by m.relatedProperty<AddressModel>(RelationType.HAS_ONE, "user_id");
 }
 
 /**
@@ -23,7 +23,7 @@ class UserModel(id:Int? = null): Orm(id) {
  */
 class AddressModel(id:Int? = null): Orm(id) {
     // 伴随用户就是元数据
-    companion object m: MetaData(AddressModel::class, relations = mapOf("user" to MetaRelation(RelationType.BELONGS_TO, UserModel::class, "user_id") /* 关联用户：一个地址从属于一个用户 */))
+    companion object m: MetaData(AddressModel::class)
 
     public var user_id:Int by m.property<Int>();
 
@@ -32,15 +32,15 @@ class AddressModel(id:Int? = null): Orm(id) {
     public var tel:String by m.property<String>();
 
     // 关联用户：一个地址从属于一个用户
-    public var user:UserModel by m.property<UserModel>();
+    public var user:UserModel by m.relatedProperty<UserModel>(RelationType.BELONGS_TO,  "user_id");
 }
 
 
 
 class OrmTests{
 
-    var id = 13;
-
+    var id = 10;
+/*
     @Test
     fun testFind(){
 //        val user = UserModel.queryBuilder().where("id", 1).find<UserModel>()
@@ -86,6 +86,53 @@ class OrmTests{
         }
         println("删除用户：$user, result: ${user.delete()}")
     }
+ */
+
+    @Test
+    fun testRelateFind(){
+        val user = UserModel(id)
+        val address = user.address
+        println(address)
+    }
+
+    @Test
+    fun testRelateCreate(){
+        val user = UserModel(id)
+        val address = AddressModel()
+        address.addr = "nanning"
+        address.tel = "110"
+        address.user = user;
+        address.create()
+        println("创建地址: $address")
+    }
+
+    @Test
+    fun testRelateUpdate(){
+        val user = UserModel(id)
+        val address = user.address
+        if(!address.isLoaded()){
+            println("用户[$user]没有地址")
+            return
+        }
+
+        address.addr = "gx"
+        address.tel = "119"
+        println("更新地址: $address")
+    }
+
+    @Test
+    fun testRelateDelete(){
+        val user = UserModel(id)
+        val address = user.address
+        if(!address.isLoaded()){
+            println("用户[$user]没有地址")
+            return
+        }
+
+        println("删除用户：$address, result: ${address.delete()}")
+    }
+
+
 }
 
 
