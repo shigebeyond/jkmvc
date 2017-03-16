2b4531a22b8c805b6831b1a78196f42868034ccd
shijianhang <772910474.com>
2017-03-16 17:57:16 +0800

    orm元数据中对属性代理做一下缓存，减少重复创建属性代理的情况

--- src/main/kotlin/com/jkmvc/orm/IMetaData.kt
+++ src/main/kotlin/com/jkmvc/orm/IMetaData.kt
@@ -1,8 +1,10 @@
 package com.jkmvc.orm
 
 import com.jkmvc.db.Db
+import java.util.concurrent.ConcurrentHashMap
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KClass
+import kotlin.reflect.KProperty
 
 /**
  * orm的元数据
@@ -10,62 +12,87 @@ import kotlin.reflect.KClass
  *  1 模型映射表的映射元数据，如模型类/数据库/表名
  *  2 代理模型的属性读写
  */
-interface IMetaData{
+abstract class IMetaData{
+
+    companion object{
+        /**
+         * 缓存属性代理
+         */
+        val props:MutableMap<KClass<*>, ReadWriteProperty<IOrm, *>> by lazy{
+            ConcurrentHashMap<KClass<*>, ReadWriteProperty<IOrm, *>>()
+        }
+    }
 
     /**
      * 模型类
      */
-    public val clazz: KClass<*>
+    public abstract val model: KClass<*>
 
     /**
      * 数据库名
      */
-    public val dbName:String
+    public abstract val dbName:String
 
     /**
      * 表名
      */
-    public var table:String
+    public abstract var table:String
 
     /**
      * 主键
      */
-    public val primaryKey:String
+    public abstract val primaryKey:String
 
     /**
      * 关联关系
      */
-    public val relations:Map<String, MetaRelation>?
+    public abstract val relations:Map<String, MetaRelation>?
 
     /**
      * 数据库
      */
-    public val db:Db
+    public abstract val db:Db
 
     /**
      * 表字段
      */
-    public val columns:List<String>
+    public abstract val columns:List<String>
 
     /**
      * 是否有某个关联关系
      */
-    public fun hasRelation(name:String):Boolean;
+    public abstract fun hasRelation(name:String):Boolean;
 
     /**
      * 获得某个关联关系
      */
-    public fun getRelation(name:String):MetaRelation?;
+    public abstract fun getRelation(name:String):MetaRelation?;
 
     /**
      * 获得orm查询构建器
      */
-    public fun queryBuilder(): OrmQueryBuilder;
+    public abstract fun queryBuilder(): OrmQueryBuilder;
 
     /**
      * 获得属性代理
      *   代理模型的属性读写
      */
-    public fun <T> property(): ReadWriteProperty<IOrm, T>;
+    public inline fun <reified T> property(): ReadWriteProperty<IOrm, T>{
+        val clazz:KClass<*> = T::class;
+        return props.getOrPut(clazz){
+            // 生成属性代理对象
+            object : ReadWriteProperty<IOrm, T> {
+                // 获得属性
+                public override operator fun getValue(thisRef: IOrm, property: KProperty<*>): T {
+                    return thisRef[property.name]
+                }
+
+                // 设置属性
+                public override operator fun setValue(thisRef: IOrm, property: KProperty<*>, value: T) {
+                    thisRef[property.name] = value
+                }
+            } as ReadWriteProperty<IOrm, *>
+        } as ReadWriteProperty<IOrm, T>;
+    }
 
 }
--- src/main/kotlin/com/jkmvc/orm/MetaData.kt
+++ src/main/kotlin/com/jkmvc/orm/MetaData.kt
@@ -1,9 +1,7 @@
 package com.jkmvc.orm
 
 import com.jkmvc.db.Db
-import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KClass
-import kotlin.reflect.KProperty
 
 /**
  * orm的元数据
@@ -11,13 +9,16 @@ import kotlin.reflect.KProperty
  *  1 模型映射表的映射元数据，如模型类/数据库/表名
  *  2 代理模型的属性读写
  */
-open class MetaData(public override val clazz: KClass<*> /* 模型类 */, public override val dbName:String = "database" /* 数据库名 */, public override var table:String = "" /* 表名 */, public override val primaryKey:String = "id" /* 主键 */, public final override val relations:Map<String, MetaRelation>? = null /* 关联关系 */ ):IMetaData{
+open class MetaData(public override val model: KClass<out IOrm> /* 模型类 */, public override val dbName:String = "database" /* 数据库名 */, public override var table:String = "" /* 表名 */, public override var primaryKey:String = "id" /* 主键 */, public final override val relations:Map<String, MetaRelation>? = null /* 关联关系 */ ):IMetaData(){
 
     init {
-        // 假定model类名, 都是以"Model"作为后缀
+        // 设置默认表名： 假定model类名, 都是以"Model"作为后缀
         if(table == "")
-            table = clazz.simpleName!!.removeSuffix("Model").toLowerCase();
+            table = model.modelName
 
+        // 设置默认的主键为id
+        if(primaryKey == "")
+            primaryKey = "id"
     }
 
     /**
@@ -52,28 +53,6 @@ open class MetaData(public override val clazz: KClass<*> /* 模型类 */, public
      */
     public override fun queryBuilder(): OrmQueryBuilder {
         return OrmQueryBuilder(this);
-    }
-
-    /**
-     * 获得属性代理
-     *   代理模型的属性读写
-     */
-    public override fun <T> property(): ReadWriteProperty<IOrm, T>{
-        return object : ReadWriteProperty<IOrm, T> {
-            /**
-             * 获得属性
-             */
-            public override operator fun getValue(thisRef: IOrm, property: KProperty<*>): T {
-                return thisRef[property.name]
-            }
-
-            /**
-             * 设置属性
-             */
-            public override operator fun setValue(thisRef: IOrm, property: KProperty<*>, value: T) {
-                thisRef[property.name] = value
-            }
-        }
     }
 }
 
