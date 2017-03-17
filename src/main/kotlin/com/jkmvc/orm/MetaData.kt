2484120b199defcaeb9f1c4850ca71947808a232
shijianhang <772910474.com>
2017-03-17 14:57:43 +0800

    完善orm metadata类中关于关联关系的设置api

--- src/main/kotlin/com/jkmvc/orm/MetaData.kt
+++ src/main/kotlin/com/jkmvc/orm/MetaData.kt
@@ -1,6 +1,7 @@
 package com.jkmvc.orm
 
 import com.jkmvc.db.Db
+import com.jkmvc.db.IDbQueryBuilder
 import java.util.concurrent.ConcurrentHashMap
 import kotlin.reflect.KClass
 
@@ -10,7 +11,7 @@ import kotlin.reflect.KClass
  *  1 模型映射表的映射元数据，如模型类/数据库/表名
  *  2 代理模型的属性读写
  */
-open class MetaData(public override val model: KClass<out IOrm> /* 模型类 */, public override val dbName:String = "database" /* 数据库名 */, public override var table:String = "" /* 表名 */, public override var primaryKey:String = "id" /* 主键 */, public final override val relations:ConcurrentHashMap<String, MetaRelation> = ConcurrentHashMap<String, MetaRelation>() /* 关联关系 */ ):IMetaData(){
+open class MetaData(public override val model: KClass<out IOrm> /* 模型类 */, public override val dbName:String = "database" /* 数据库名 */, public override var table:String = "" /* 表名 */, public override var primaryKey:String = "id" /* 主键 */, public final override val relations:ConcurrentHashMap<String, MetaRelation> = ConcurrentHashMap<String, MetaRelation>() /* 关联关系 */ ):IMetaData{
 
     init {
         // 设置默认表名： 假定model类名, 都是以"Model"作为后缀
@@ -27,6 +28,12 @@ open class MetaData(public override val model: KClass<out IOrm> /* 模型类 */,
      */
     public override val db:Db
         get() = Db.getDb(dbName)
+
+    /**
+     * 默认外键
+     */
+    public override val defaultForeignKey:String
+        get() = model.modelName + '_' + primaryKey;
 
     /**
      * 表字段
@@ -55,5 +62,58 @@ open class MetaData(public override val model: KClass<out IOrm> /* 模型类 */,
     public override fun queryBuilder(): OrmQueryBuilder {
         return OrmQueryBuilder(this);
     }
+
+    /**
+     * 生成属性代理 + 设置关联关系(belongs to)
+     */
+    public override fun belongsTo(name:String, relatedModel: KClass<out IOrm>, foreignKey:String, conditions:((IDbQueryBuilder) -> Unit)?): IMetaData {
+        // 获得外键
+        var fk = foreignKey;
+        if(fk == "")
+            fk = relatedModel.modelMetaData.defaultForeignKey
+
+        // 设置关联关系
+        relations.getOrPut(name){
+            MetaRelation(RelationType.BELONGS_TO, relatedModel, fk, conditions)
+        }
+
+        return this;
+    }
+
+    /**
+     * 设置关联关系(has one)
+     */
+    public override fun hasOne(name:String, relatedModel: KClass<out IOrm>, foreignKey:String, conditions:((IDbQueryBuilder) -> Unit)? ): IMetaData {
+        // 获得外键
+        var fk = foreignKey;
+        if(fk == "")
+            fk = this.defaultForeignKey
+
+
+        // 设置关联关系
+        relations.getOrPut(name){
+            MetaRelation(RelationType.HAS_ONE, relatedModel, fk, conditions)
+        }
+
+        return this;
+    }
+
+    /**
+     * 设置关联关系(has many)
+     */
+    public override fun hasMany(name:String, relatedModel: KClass<out IOrm>, foreignKey:String, conditions:((IDbQueryBuilder) -> Unit)?): IMetaData {
+        // 获得外键
+        var fk = foreignKey;
+        if(fk == "")
+            fk = this.defaultForeignKey
+
+        // 设置关联关系
+        relations.getOrPut(name){
+            MetaRelation(RelationType.HAS_MANY, relatedModel, fk, conditions)
+        }
+
+        return this;
+    }
+
 }
 
