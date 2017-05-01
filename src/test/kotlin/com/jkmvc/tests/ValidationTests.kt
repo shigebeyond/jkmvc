cfcb52b54af6b23563d3f87ebaa7c3b57aa38584
shijianhang <3076679995@qq.com>
2017-05-01 22:48:37 +0800

    添加校验测试

--- src/test/kotlin/com/jkmvc/tests/ValidationTests.kt
+++ src/test/kotlin/com/jkmvc/tests/ValidationTests.kt
@@ -1,5 +1,6 @@
 package com.jkmvc.tests
 
+import com.jkmvc.validate.ValidationExpression
 import org.junit.Test
 
 class ValidationTests{
@@ -7,7 +8,7 @@ class ValidationTests{
 
     @Test
     fun testValidate(){
-        val exp = ValidationExpression("trim . toUpperCase . substring(2)");
+        val exp = ValidationExpression("trim . toUpperCase . substring(2,-1)");
         val (value, message) = exp.execute(" model ");
         println(value)
     }
