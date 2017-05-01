bfb5979de77fe5082868b1c6dcda5f054ac630e5
shijianhang <3076679995@qq.com>
2017-05-01 22:48:22 +0800

    添加反射测试

--- src/test/kotlin/com/jkmvc/tests/MyTests.kt
+++ src/test/kotlin/com/jkmvc/tests/MyTests.kt
@@ -1,12 +1,8 @@
 package com.jkmvc.tests
 
-import com.jkmvc.db.Db
-import com.jkmvc.db.DbQueryBuilder
-import com.jkmvc.db.Record
-import com.jkmvc.tests.UserModel
+import com.jkmvc.common.to
+import com.jkmvc.validate.Validation
 import org.junit.Test
-import java.util.concurrent.ConcurrentHashMap
-import kotlin.reflect.jvm.jvmName
 
 open class A() {}
 class B():A() {}
@@ -20,6 +16,8 @@ fun B.echo(){
 }
 
 class MyTests{
+
+
 
   /*  @Test
     fun testClassName(){
@@ -36,7 +34,7 @@ class MyTests{
         b.echo() // B
         val c:A = B()
         c.echo() // A
-    }*/
+    }
 
     @Test
     fun testMeta(){
@@ -47,6 +45,48 @@ class MyTests{
 //        println(UserModel.m::class.java.typeName) // com.jkmvc.tests.UserModel$m
 //        println(UserModel.m::class.java.canonicalName) // com.jkmvc.tests.UserModel.m -- dot
 //        println(UserModel.m::class.java.simpleName) // m
+    }
+
+    @Test
+    fun testFunc(){
+        val f: Lambda = { it:String ->
+            it
+        } as Lambda
+        println(f.javaClass)
+        println(f.javaClass.kotlin)
+        println(f is KFunction<*>) // false
+        println(f is KCallable<*>) // false
+        println(f is Lambda) // true
+        println(f.javaClass.superclass) // class kotlin.jvm.internal.Lambda
+        println(f.javaClass.superclass.superclass) // Object
+    }
+
+    @Test
+    fun testMember(){
+        // kotlin.reflect.jvm.internal.KotlinReflectionInternalError: Reflection on built-in Kotlin types is not yet fully supported. No metadata found for public open val length: kotlin.Int defined in kotlin.String
+        /*
+        for(m in  String::class.declaredFunctions)
+            println(m.name)
+        */
+        for(m in String.javaClass.methods)
+            println(m.name)
+    }
+    */
+
+    @Test
+    fun testType(){
+        val type = Validation::trim.parameters[0].type
+        println(type::class)
+        println(type.javaClass)
+        println(type.classifier)
+    }
+
+    @Test
+    fun testTo(){
+        println("123".to(Int::class))
+        println("123.45".to(Float::class))
+        println("123.4567".to(Double::class))
+        println("true".to(Boolean::class))
     }
 }
 
