8f24cccb39a511fadf404f92c0fdc85175db5c3d
shijianhang <3076679995@qq.com>
2017-05-09 00:19:44 +0800

    添加另外的测试controller

--- src/main/kotlin/com/jkmvc/example/UserController.kt
+++ src/main/kotlin/com/jkmvc/example/UserController.kt
@@ -4,10 +4,10 @@ import com.jkmvc.http.Controller
 import com.jkmvc.http.Request
 import com.jkmvc.http.Response
 
-class WelcomeController(req: Request /* 请求对象 */, res: Response /* 响应对象 */): Controller(req, res) {
+class UserController(req: Request /* 请求对象 */, res: Response /* 响应对象 */): Controller(req, res) {
 
     public fun actionIndex(){
-        res.render("hello world");
+        res.render("hello user");
     }
 
 }
