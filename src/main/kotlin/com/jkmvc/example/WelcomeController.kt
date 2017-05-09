2b886b90f864980a1f2fa398a4d1f28a59275444
shijianhang <772910474.com>
2017-05-09 17:16:22 +0800

    完善jsp视图的渲染

--- src/main/kotlin/com/jkmvc/example/WelcomeController.kt
+++ src/main/kotlin/com/jkmvc/example/WelcomeController.kt
@@ -3,11 +3,16 @@ package com.jkmvc.example
 import com.jkmvc.http.Controller
 import com.jkmvc.http.Request
 import com.jkmvc.http.Response
+import com.jkmvc.http.View
 
 class WelcomeController(req: Request /* 请求对象 */, res: Response /* 响应对象 */): Controller(req, res) {
 
-    public fun actionIndex(){
+    public fun actionIndex() {
         res.render("hello world");
+    }
+
+    public fun actionJsp(){
+        res.render(view("index", mutableMapOf("name" to "shijianhang")))
     }
 
 }
