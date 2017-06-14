f85b342abffe329dafa0ee43497326fe2f1d6807
shijianhang <772910474.com>
2017-06-14 18:29:08 +0800

    编译sql中支持构建批量更新的参数

--- src/main/kotlin/com/jkmvc/db/CompiledSql.kt
+++ src/main/kotlin/com/jkmvc/db/CompiledSql.kt
@@ -6,7 +6,9 @@ import kotlin.collections.ArrayList
 
 /**
  * 编译好的sql
- *   在预编译的场景下，该编译好的sql会被缓存，其属性 sql/params/dynamicParams 方法debugSql() 有可能会被调用多次
+ *   在预编译的场景下，该编译好的sql会被缓存，其属性 sql/staticParams/dynamicParams 方法debugSql() 有可能会被调用多次
+ *   动态参数，是指静态参数中为?的参数
+ *   如果你的实际参数指就是?，请使用静态参数，不要使用动态参数（就是buildParams()中参数为空），因为这样动态参数的个数是对不上的
  *
  * @author shijianhang
  * @date 2017-6-10 下午8:02:47
@@ -31,31 +33,16 @@ class CompiledSql : Cloneable, ICompiledSql {
     public override var staticParams: LinkedList<Any?> = LinkedList<Any?>()
 
     /**
-     * 构建实际参数 = 静态参数 + 动态参数
-     *
-     * @param params 动态参数
-     * @return
+     * 动态参数的个数 = 静态参数中?的个数
      */
-    public override fun buildParams(dynamicParams:Array<out Any?>):List<Any?>{
-        if(dynamicParams.isEmpty())
-            return staticParams;
-
-        // 构建实际参数：将静态参数中?，替换为动态参数
-        val realParams = ArrayList<Any?>()
-        var i = 0; // 动态变量的迭代索引
-        for (v in staticParams){
-            if(v is String && v == "?") // 如果参数值是?，则认为是动态参数
-                realParams.add(dynamicParams!![i++])
-            else // 静态参数
-                realParams.add(v)
+    public override val dynamicParamsSize:Int
+        get(){
+            var size = 0;
+            for(param in staticParams)
+                if(param is String && param == "?")
+                    size++;
+            return size
         }
-
-        // 预览sql
-        if(Db.debug)
-            println("实际的sql：" + previewSql(dynamicParams))
-
-        return realParams;
-    }
 
     /**
      * 判断是否为空
@@ -69,7 +56,7 @@ class CompiledSql : Cloneable, ICompiledSql {
      * 清空编译结果
      * @return
      */
-    override fun clear(): CompiledSql {
+    override fun clear(): ICompiledSql {
         sql = "";
         staticParams.clear();
         // dynamicParams = null;
@@ -87,20 +74,93 @@ class CompiledSql : Cloneable, ICompiledSql {
     }
 
     /**
+     * 构建实际参数 = 静态参数 + 动态参数
+     *
+     * @param dynamicParamses 动态参数
+     * @return
+     */
+    public override fun buildParams(dynamicParams:List<Any?>):List<Any?>{
+        if(dynamicParams.isEmpty())
+            return staticParams;
+
+        // 检查动态参数个数
+        val size = dynamicParamsSize;
+        if(dynamicParams.size != size)
+            throw DbException("动态参数个数不对：需要 $size 个，传递 ${dynamicParams.size} ");
+
+        // 构建实际参数：将静态参数中?，替换为动态参数
+        return collectParams(ArrayList<Any?>(staticParams.size), dynamicParams)
+    }
+
+    /**
+     * 收集实际参数 = 静态参数 + 动态参数
+     *
+     * @param dynamicParamses 动态参数
+     * @param fromIndex 动态参数的开始索引
+     * @return
+     */
+    protected fun collectParams(result: ArrayList<Any?>, dynamicParams: List<Any?>, fromIndex:Int = 0): ArrayList<Any?> {
+        // 构建实际参数：将静态参数中?，替换为动态参数
+        var i = 0; // 动态变量的迭代索引
+        for (v in staticParams) {
+            if (v is String && v == "?") // 如果参数值是?，则认为是动态参数
+                result.add(dynamicParams[fromIndex + (i++)])
+            else // 静态参数
+                result.add(v)
+        }
+
+        // 预览sql
+        if(Db.debug)
+            println("实际的sql：" + previewSql(dynamicParams, fromIndex))
+
+        return result;
+    }
+
+    /**
+     * 构建实际参数 = 静态参数 + 动态参数
+     *
+     * @param dynamicParamses 多次处理的动态参数的汇总，一次处理取 paramSize 个参数，必须保证他的大小是 paramSize 的整数倍
+     * @param paramSize 一次处理的参数个数
+     * @return
+     */
+    public override fun buildBatchParamses(dynamicParamses: List<Any?>, paramSize: Int):List<Any?>{
+        // 检查动态参数个数
+        val size = dynamicParamsSize;
+        if(paramSize != size)
+            throw DbException("动态参数个数不对：需要 $size 个，传递 $paramSize ");
+
+        // 计算批处理的次数
+        if(paramSize <= 0)
+            throw Exception("paramSize 只能为正整数");
+        if(dynamicParamses.size % paramSize > 0)
+            throw Exception("paramses 的大小必须是 paramSize 的整数倍");
+        val batchNum:Int = dynamicParamses.size / paramSize
+
+        // 构建实际参数：将静态参数中?，替换为动态参数
+        val realParams = ArrayList<Any?>(staticParams.size * batchNum)
+        for(i in 0..(batchNum - 1)){
+            collectParams(realParams, dynamicParamses, i * paramSize)
+        }
+
+        return realParams;
+    }
+
+    /**
      * 预览sql
      *
      * @param params 动态参数
+     * @param fromIndex 动态参数的开始索引
      * @return
      */
-    public override fun previewSql(dynamicParams:Array<out Any?>?): String {
+    public override fun previewSql(dynamicParams: List<Any?>, fromIndex: Int): String {
         // 替换实参
         var i = 0 // 静态变量的迭代索引
-        var j = 0 // 动态变量的迭代索引
+        var j = fromIndex // 动态变量的迭代索引
         val realSql = sql.replace("\\?".toRegex()) { matches: MatchResult ->
             val param = staticParams[i++] // 静态参数
             if(param is String){
-                if(param == "?" && dynamicParams != null)// 如果参数值是?，则认为是动态参数
-                    dynamicParams!![j++].toString()
+                if(param == "?" && dynamicParams.isNotEmpty())// 如果参数值是?，则认为是动态参数
+                    dynamicParams[j++].toString()
                 else
                     "\"$param\""
             } else
--- src/main/kotlin/com/jkmvc/db/ICompiledSql.kt
+++ src/main/kotlin/com/jkmvc/db/ICompiledSql.kt
@@ -17,15 +17,12 @@ interface ICompiledSql {
     /**
      * 编译后的sql参数 / 静态参数
      */
-    public var staticParams: LinkedList<Any?>
+    var staticParams: LinkedList<Any?>
 
     /**
-     * 构建实际参数 = 静态参数 + 动态参数
-     *
-     * @param params 动态参数
-     * @return
+     * 动态参数的个数 = 静态参数中?的个数
      */
-    fun buildParams(dynamicParams: Array<out Any?>): List<Any?>
+    val dynamicParamsSize:Int;
 
     /**
      * 判断是否为空
@@ -37,13 +34,41 @@ interface ICompiledSql {
      * 清空编译结果
      * @return
      */
-    fun clear(): CompiledSql
+    fun clear(): ICompiledSql
+
+    /**
+     * 构建实际参数 = 静态参数 + 动态参数
+     *
+     * @param params 动态参数
+     * @return
+     */
+    fun buildParams(dynamicParams: List<Any?> = emptyList()): List<Any?>
+
+    /**
+     * 构建实际参数 = 静态参数 + 动态参数
+     *
+     * @param params 动态参数
+     * @return
+     */
+    fun buildParams(dynamicParams: Array<out Any?>): List<Any?>{
+       return buildParams(dynamicParams.asList())
+    }
+
+    /**
+     * 构建实际参数 = 静态参数 + 动态参数
+     *
+     * @param dynamicParamses 多次处理的动态参数的汇总，一次处理取 paramSize 个参数，必须保证他的大小是 paramSize 的整数倍
+     * @param paramSize 一次处理的参数个数
+     * @return
+     */
+    fun buildBatchParamses(dynamicParamses: List<Any?>, paramSize: Int):List<Any?>;
 
     /**
      * 预览sql
      *
      * @param params 动态参数
+     * @param fromIndex 动态参数的开始索引
      * @return
      */
-    fun previewSql(dynamicParams:Array<out Any?>? = null): String
+    fun previewSql(dynamicParams:List<Any?> = emptyList(), fromIndex:Int = 0): String
 }
