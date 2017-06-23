0e70bfa041a141e0789387e3ca5b27878492fbb2
shijianhang <3076679995@qq.com>
2017-06-23 22:12:43 +0800

    com.oreilly.servlet.MultipartRequest扩展

--- src/main/kotlin/com/jkmvc/http/_Request.kt
+++ src/main/kotlin/com/jkmvc/http/_Request.kt
@@ -1,5 +1,32 @@
 package com.jkmvc.http
 
+import com.jkmvc.common.findProperty
+import java.util.Enumeration
+import java.util.HashMap
+import com.oreilly.servlet.MultipartRequest
+import kotlin.reflect.KProperty1
+
+/****************************** com.oreilly.servlet.MultipartRequest扩展 *******************************/
 /**
- * Created by shi on 17-6-23.
+ * 上传参数的属性
  */
+val parametersProp: KProperty1<MultipartRequest, Map<String, Array<String>>> = (MultipartRequest::class.findProperty("parameters") as KProperty1<MultipartRequest, Map<String, Array<String>>>?)!!
+
+/**
+ * 获得上传参数
+ */
+public fun MultipartRequest.getParameterMap(): Map<String, Array<String>> {
+    return parametersProp.get(this)
+}
+
+/**
+ * 上传文件的属性
+ */
+val filesProp: KProperty1<MultipartRequest, Map<String, Array<String>>> = (MultipartRequest::class.findProperty("files") as KProperty1<MultipartRequest, Map<String, Array<String>>>?)!!
+
+/**
+ * 获得上传文件
+ */
+public fun MultipartRequest.getFileMap(): Map<String, Array<String>> {
+    return filesProp.get(this)
+}
