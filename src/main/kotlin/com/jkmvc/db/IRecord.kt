9b2c1267983b2488dac9fdf52fcdf003e48af6f1
shijianhang <772910474.com>
2017-07-05 15:29:01 +0800

    为record类添加getShort方法

--- src/main/kotlin/com/jkmvc/db/IRecord.kt
+++ src/main/kotlin/com/jkmvc/db/IRecord.kt
@@ -22,91 +22,70 @@ interface IRecord {
     /**
      * Get attribute of mysql type: varchar, char, enum, set, text, tinytext, mediumtext, longtext
      */
-    public fun getStr(name: String): String {
-        return this[name]
-    }
+    fun getString(name: String): String
 
     /**
      * Get attribute of mysql type: int, integer, tinyint(n) n > 1, smallint, mediumint
      */
-    public fun getInt(name: String): Int? {
-        return this[name]
-    }
+    fun getInt(name: String): Int?
 
     /**
      * Get attribute of mysql type: bigint, unsign int
      */
-    public fun getLong(name: String): Long? {
-        return this[name]
-    }
+    fun getLong(name: String): Long?
 
     /**
      * Get attribute of mysql type: unsigned bigint
      */
-    public fun getBigInteger(name: String): java.math.BigInteger {
-        return this[name]
-    }
+    fun getBigInteger(name: String): java.math.BigInteger
 
     /**
      * Get attribute of mysql type: date, year
      */
-    public fun getDate(name: String): java.util.Date {
-        return this[name]
-    }
+    fun getDate(name: String): java.util.Date
 
     /**
      * Get attribute of mysql type: time
      */
-    public fun getTime(name: String): java.sql.Time {
-        return this[name]
-    }
+    fun getTime(name: String): java.sql.Time
 
     /**
      * Get attribute of mysql type: timestamp, datetime
      */
-    public fun getTimestamp(name: String): java.sql.Timestamp {
-        return this[name]
-    }
+    fun getTimestamp(name: String): java.sql.Timestamp
 
     /**
      * Get attribute of mysql type: real, double
      */
-    public fun getDouble(name: String): Double? {
-        return this[name]
-    }
+    fun getDouble(name: String): Double?
 
     /**
      * Get attribute of mysql type: float
      */
-    public fun getFloat(name: String): Float? {
-        return this[name]
-    }
+    fun getFloat(name: String): Float?
 
     /**
      * Get attribute of mysql type: bit, tinyint(1)
      */
-    public fun getBoolean(name: String): Boolean? {
-        return this[name]
-    }
+    fun getBoolean(name: String): Boolean?
+
+    /**
+     * Get attribute of mysql type: tinyint(1)
+     */
+    fun getShort(name: String): Short?
 
     /**
      * Get attribute of mysql type: decimal, numeric
      */
-    public fun getBigDecimal(name: String): java.math.BigDecimal {
-        return this[name]
-    }
+    fun getBigDecimal(name: String): java.math.BigDecimal
 
     /**
      * Get attribute of mysql type: binary, varbinary, tinyblob, blob, mediumblob, longblob
      */
-    public fun getBytes(name: String): ByteArray {
-        return this[name]
-    }
+    fun getBytes(name: String): ByteArray
 
     /**
      * Get attribute of any type that extends from Number
      */
-    public fun getNumber(name: String): Number {
-        return this[name]
-    }
+    fun getNumber(name: String): Number
 }
--- src/main/kotlin/com/jkmvc/db/Record.kt
+++ src/main/kotlin/com/jkmvc/db/Record.kt
@@ -24,6 +24,104 @@ class Record(protected val data: MutableMap<String, Any?> = HashMap<String, Any?
         return (data.get(name) ?: defaultValue) as T
     }
 
+    /**
+     * Get attribute of mysql type: varchar, char, enum, set, text, tinytext, mediumtext, longtext
+     */
+    public override fun getString(name: String): String {
+        return this[name]
+    }
+
+    /**
+     * Get attribute of mysql type: int, integer, tinyint(n) n > 1, smallint, mediumint
+     */
+    public override fun getInt(name: String): Int? {
+        return this[name]
+    }
+
+    /**
+     * Get attribute of mysql type: bigint, unsign int
+     */
+    public override fun getLong(name: String): Long? {
+        return this[name]
+    }
+
+    /**
+     * Get attribute of mysql type: unsigned bigint
+     */
+    public override fun getBigInteger(name: String): java.math.BigInteger {
+        return this[name]
+    }
+
+    /**
+     * Get attribute of mysql type: date, year
+     */
+    public override fun getDate(name: String): java.util.Date {
+        return this[name]
+    }
+
+    /**
+     * Get attribute of mysql type: time
+     */
+    public override fun getTime(name: String): java.sql.Time {
+        return this[name]
+    }
+
+    /**
+     * Get attribute of mysql type: timestamp, datetime
+     */
+    public override fun getTimestamp(name: String): java.sql.Timestamp {
+        return this[name]
+    }
+
+    /**
+     * Get attribute of mysql type: real, double
+     */
+    public override fun getDouble(name: String): Double? {
+        return this[name]
+    }
+
+    /**
+     * Get attribute of mysql type: float
+     */
+    public override fun getFloat(name: String): Float? {
+        return this[name]
+    }
+
+    /**
+     * Get attribute of mysql type: bit, tinyint(1)
+     */
+    public override fun getBoolean(name: String): Boolean? {
+        return this[name]
+    }
+
+    /**
+     * Get attribute of mysql type: tinyint(1)
+     */
+    public override fun getShort(name: String): Short? {
+        return this[name]
+    }
+
+    /**
+     * Get attribute of mysql type: decimal, numeric
+     */
+    public override fun getBigDecimal(name: String): java.math.BigDecimal {
+        return this[name]
+    }
+
+    /**
+     * Get attribute of mysql type: binary, varbinary, tinyblob, blob, mediumblob, longblob
+     */
+    public override fun getBytes(name: String): ByteArray {
+        return this[name]
+    }
+
+    /**
+     * Get attribute of any type that extends from Number
+     */
+    public override fun getNumber(name: String): Number {
+        return this[name]
+    }
+
     public override fun toString(): String {
         return "${this.javaClass}: " + data.toString()
     }
