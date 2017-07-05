ecf4b275ff82aa33fdf562d9a9f0b66c3f761573
shijianhang <772910474.com>
2017-07-05 15:31:08 +0800

    为编译好的sql CompiledSql 添加各种查询/执行sql的方法，以便复用编译好的sql来重复执行查询

--- src/main/kotlin/com/jkmvc/db/CompiledSql.kt
+++ src/main/kotlin/com/jkmvc/db/CompiledSql.kt
@@ -1,20 +1,22 @@
 package com.jkmvc.db
 
+import com.jkmvc.common.defaultValue
 import java.util.*
 import kotlin.collections.ArrayList
 
 
 /**
  * 编译好的sql
- *   在预编译的场景下，该编译好的sql会被缓存，其属性 sql/staticParams/dynamicParams 方法debugSql() 有可能会被调用多次
+ *   为了避免多次编译，可以缓存该编译好的sql，其属性 sql/staticParams/dynamicParams 方法debugSql() 有可能会被调用多次
  *   动态参数，是指静态参数中为?的参数
  *   如果你的实际参数指就是?，请使用静态参数，不要使用动态参数（就是buildParams()中参数为空），因为这样动态参数的个数是对不上的
  *
  * @author shijianhang
  * @date 2017-6-10 下午8:02:47
  */
-class CompiledSql : Cloneable, ICompiledSql {
+class CompiledSql(public override val dbName: String = "default" /* 数据库名 */) : Cloneable, ICompiledSql {
 
+    /****************************** 构建参数/sql *******************************/
     /**
      * 编译好的sql
      */
@@ -43,6 +45,14 @@ class CompiledSql : Cloneable, ICompiledSql {
                     size++;
             return size
         }
+
+    /**
+     * 数据库连接
+     *   由于 CompiledSql 作为编译后的sql可以被缓存起来，以便复用，避免重复编译sql，因此一个 CompiledSql 对象可以跨越多个请求/线程而存在的，从而导致他的属性不能有只存在于单个请求/线程的对象，如数据库连接（上一个请求与下一个请求获得的连接可以不一样）
+     *   => 只记录dbName，每次执行都获得当前线程最新的连接
+     */
+    public override val db: IDb
+        get() = Db.getDb(dbName)
 
     /**
      * 判断是否为空
@@ -95,6 +105,7 @@ class CompiledSql : Cloneable, ICompiledSql {
     /**
      * 收集实际参数 = 静态参数 + 动态参数
      *
+     * @param result 实际参数
      * @param dynamicParamses 动态参数
      * @param fromIndex 动态参数的开始索引
      * @return
@@ -168,4 +179,145 @@ class CompiledSql : Cloneable, ICompiledSql {
         }
         return realSql
     }
+
+    /****************************** 执行与查询sql *******************************/
+    /**
+     * 查找多个： select 语句
+     *
+     * @param params 动态参数
+     * @param transform 转换函数
+     * @return 列表
+     */
+    public override fun <T:Any> findAll(vararg params: Any?, transform:(MutableMap<String, Any?>) -> T): List<T>{
+        // 执行 select
+        return db.queryRows<T>(sql, buildParams(params), transform)
+    }
+
+    /**
+     * 查找一个： select ... limit 1语句
+     *
+     * @param params 动态参数
+     * @param transform 转换函数
+     * @return 单个数据
+     */
+    public override fun <T:Any> find(vararg params: Any?, transform:(MutableMap<String, Any?>) -> T): T?{
+        // 执行 select limit 1
+        var psql = sql;
+        if(!psql.contains("LIMIT"))
+            psql += " LIMIT 1"
+        return db.queryRow<T>(sql, buildParams(params), transform);
+    }
+
+    /**
+     * 查询一个值（单行单列）
+     *
+     * @param params 动态参数
+     * @return
+     */
+    public inline fun <reified T:Any> findValue(params: Array<out Any?>):T{
+        // 执行 select
+        val (hasNext, count) = db.queryCell(sql, buildParams(params));
+        return if(hasNext)
+            count as T;
+        else
+            T::class.defaultValue
+    }
+
+    /**
+     * 查询一个值（单行单列）
+     *
+     * @param params 动态参数
+     * @return
+     */
+    public override fun findInt(vararg params: Any?):Int {
+        return findValue(params)
+    }
+
+    /**
+     * 查询一个值（单行单列）
+     *
+     * @param params 动态参数
+     * @return
+     */
+    public override fun findLong(vararg params: Any?):Long {
+        return findValue(params)
+    }
+
+    /**
+     * 查询一个值（单行单列）
+     *
+     * @param params 动态参数
+     * @return
+     */
+    public override fun findFloat(vararg params: Any?):Float {
+        return findValue(params)
+    }
+
+    /**
+     * 查询一个值（单行单列）
+     *
+     * @param params 动态参数
+     * @return
+     */
+    public override fun findDouble(vararg params: Any?):Double {
+        return findValue(params)
+    }
+
+    /**
+     * 查询一个值（单行单列）
+     *
+     * @param params 动态参数
+     * @return
+     */
+    public override fun findBoolean(vararg params: Any?):Boolean {
+        val i:Int = findValue(params)
+        return i > 0;
+    }
+
+    /**
+     * 查询一个值（单行单列）
+     *
+     * @param params 动态参数
+     * @return
+     */
+    public override fun findByte(vararg params: Any?):Byte {
+        return findValue(params)
+    }
+
+    /**
+     * 查询一个值（单行单列）
+     *
+     * @param params 动态参数
+     * @return
+     */
+    public override fun findShort(vararg params: Any?):Short {
+        return findValue(params)
+    }
+
+    /**
+     * 编译 + 执行
+     *
+     * @param params 动态参数
+     * @param returnGeneratedKey 是否返回自动生成的主键
+     * @return 影响行数|新增id
+     */
+    public override fun execute(params:Array<out Any?>, returnGeneratedKey:Boolean):Int
+    {
+        // 2 执行sql
+        return db.execute(sql, buildParams(params), returnGeneratedKey);
+    }
+
+    /**
+     * 批量更新有参数的sql
+     *
+     * @param action sql动作：select/insert/update/delete
+     * @param paramses 多次处理的参数的汇总，一次处理取 paramSize 个参数，必须保证他的大小是 paramSize 的整数倍
+     * @param paramSize 一次处理的参数个数
+     * @return
+     */
+    public override fun batchExecute(paramses: List<Any?>, paramSize:Int): IntArray {
+        // 批量执行有参数sql
+        return db.batchExecute(sql, buildBatchParamses(paramses, paramSize), staticParams.size);
+    }
+
 }
--- src/main/kotlin/com/jkmvc/db/ICompiledSql.kt
+++ src/main/kotlin/com/jkmvc/db/ICompiledSql.kt
@@ -9,6 +9,13 @@ import java.util.*
  * @date 2017-6-10 下午8:02:47
  */
 interface ICompiledSql {
+
+    /****************************** 构建参数/sql *******************************/
+    /**
+     * 数据库名
+     */
+    val dbName:String
+
     /**
      * 编译好的sql
      */
@@ -23,6 +30,13 @@ interface ICompiledSql {
      * 动态参数的个数 = 静态参数中?的个数
      */
     val dynamicParamsSize:Int;
+
+    /**
+     * 数据库连接
+     *   由于 CompiledSql 作为编译后的sql可以被缓存起来，以便复用，避免重复编译sql，因此一个 CompiledSql 对象可以跨越多个请求/线程而存在的，从而导致他的属性不能有只存在于单个请求/线程的对象，如数据库连接（上一个请求与下一个请求获得的连接可以不一样）
+     *   => 只记录dbName，每次执行都获得当前线程最新的连接
+     */
+    val db: IDb
 
     /**
      * 判断是否为空
@@ -51,7 +65,7 @@ interface ICompiledSql {
      * @return
      */
     fun buildParams(dynamicParams: Array<out Any?>): List<Any?>{
-       return buildParams(dynamicParams.asList())
+        return buildParams(dynamicParams.asList())
     }
 
     /**
@@ -71,4 +85,97 @@ interface ICompiledSql {
      * @return
      */
     fun previewSql(dynamicParams:List<Any?> = emptyList(), fromIndex:Int = 0): String
+
+    /****************************** 执行与查询sql *******************************/
+    /**
+     * 查找多个记录： select 语句
+     *
+     * @param params 动态参数
+     * @param fun transform 转换函数
+     * @return 列表
+     */
+    fun <T:Any> findAll(vararg params: Any?, transform:(MutableMap<String, Any?>) -> T): List<T>;
+
+    /**
+     * 查找一个记录： select ... limit 1语句
+     *
+     * @param params 动态参数
+     * @param fun transform 转换函数
+     * @return 一个数据
+     */
+    fun <T:Any> find(vararg params: Any?, transform:(MutableMap<String, Any?>) -> T): T?;
+
+    /**
+     * 查询一个值（单行单列）
+     *
+     * @param params 动态参数
+     * @return
+     */
+    fun findInt(vararg params: Any?):Int
+
+    /**
+     * 查询一个值（单行单列）
+     *
+     * @param params 动态参数
+     * @return
+     */
+    fun findLong(vararg params: Any?):Long
+
+    /**
+     * 查询一个值（单行单列）
+     *
+     * @param params 动态参数
+     * @return
+     */
+    fun findFloat(vararg params: Any?):Float
+
+    /**
+     * 查询一个值（单行单列）
+     *
+     * @param params 动态参数
+     * @return
+     */
+    fun findDouble(vararg params: Any?):Double
+
+    /**
+     * 查询一个值（单行单列）
+     *
+     * @param params 动态参数
+     * @return
+     */
+    fun findBoolean(vararg params: Any?):Boolean
+
+    /**
+     * 查询一个值（单行单列）
+     *
+     * @param params 动态参数
+     * @return
+     */
+    fun findByte(vararg params: Any?):Byte
+
+    /**
+     * 查询一个值（单行单列）
+     *
+     * @param params 动态参数
+     * @return
+     */
+    fun findShort(vararg params: Any?):Short
+
+    /**
+     * 编译 + 执行
+     *
+     * @param params 动态参数
+     * @param returnGeneratedKey 是否返回自动生成的主键
+     * @return 影响行数|新增id
+     */
+    fun execute(params:Array<out Any?>, returnGeneratedKey:Boolean = false):Int;
+
+    /**
+     * 批量更新有参数的sql
+     *
+     * @param paramses 多次处理的参数的汇总，一次处理取 paramSize 个参数，必须保证他的大小是 paramSize 的整数倍
+     * @param paramSize 一次处理的参数个数
+     * @return
+     */
+    fun batchExecute(paramses: List<Any?>, paramSize:Int): IntArray;
 }
