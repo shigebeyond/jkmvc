4ac36990ce86d6f88f10862a8985839ff5578d7f
shijianhang <3076679995@qq.com>
2017-09-16 22:58:34 +0800

    fix bug: 校验方法notEmpty

--- jkmvc-common/src/main/kotlin/com/jkmvc/validate/Validation.kt
+++ jkmvc-common/src/main/kotlin/com/jkmvc/validate/Validation.kt
@@ -17,7 +17,7 @@ object Validation:IValidation
 	 * 校验方法对应的错误消息
 	 */
 	public val messages:Map<String, String> = mapOf(
-			"notempty" to "不能为空",
+			"notEmpty" to "不能为空",
 			"length" to "的长度必须在:0到:1之间",
 			"range" to "的数值必须是:0到:1之间的整数",
 			"min" to "的数值必须不少于:0",
