8152793be42b75e31f2894ed753a467c2e7d2052
shijianhang <772910474.com>
2017-10-09 18:58:00 +0800

    添加帮助方法

--- jkmvc-orm/src/main/kotlin/com/jkmvc/orm/_Orm.kt
+++ jkmvc-orm/src/main/kotlin/com/jkmvc/orm/_Orm.kt
@@ -28,8 +28,57 @@ public val KClass<out IOrm>.modelOrmMeta: IOrmMeta
 /**
  * orm列表获得字段值
  */
-fun List<out IOrm>.itemAsMap(): List<Map<String, Any?>> {
+fun Collection<out IOrm>.itemAsMap(): List<Map<String, Any?>> {
     return this.map {
         it.asMap()
+    }
+}
+
+/**
+ * orm列表获得字段值
+ */
+fun Collection<out IOrm>.asMap(): List<Map<String, Any?>> {
+    return this.map {
+        it.asMap()
+    }
+}
+
+/**
+ * orm列表转哈希
+ *
+ * @param keyField 子项字段名，其值作为结果哈希的key
+ * @param valueField 子项字段名，其值作为结果哈希的value，如果为null，则用子项作为结果哈希的value
+ * @return
+ */
+fun <K, V> Collection<out IOrm>.asMap(keyField:String, valueField:String?): Map<K, V?> {
+    val result = HashMap<K, V?>()
+    this.forEach {
+        val key:K = it[keyField]
+        val value:V = if(valueField == null) it as V else it[valueField]
+        result[key] = value
+    }
+    return result
+}
+
+/**
+ * orm列表转哈希
+ *
+ * @param keyField 字段名，其值作为结果哈希的key
+ * @return
+ */
+fun <K, V:IOrm> Collection<V>.asMap(keyField:String): Map<K, V> {
+    return asMap<K, V>(keyField, null) as Map<K, V>
+}
+
+/**
+ * 收集某列的值
+ *
+ * @param key
+ * @return
+ */
+public fun Collection<out IOrm>.collectColumn(key:String):Collection<Any?>{
+    return this.map {
+        val v: Any? = it[key]
+        v
     }
 }
