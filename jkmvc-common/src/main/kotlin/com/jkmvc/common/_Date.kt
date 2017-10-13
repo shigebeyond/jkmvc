c831a986dc94b8f13665a150a2b3a637499860d6
shijianhang <772910474.com>
2017-10-13 18:51:26 +0800

    添加Date帮助方法来获得一天的开始时间与结束时间

--- jkmvc-common/src/main/kotlin/com/jkmvc/common/_Date.kt
+++ jkmvc-common/src/main/kotlin/com/jkmvc/common/_Date.kt
@@ -3,7 +3,6 @@ package com.jkmvc.common
 import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.ConcurrentHashMap
-import kotlin.collections.HashMap
 
 /**
  * 缓存日期格式
@@ -22,4 +21,34 @@ public fun Date.format(pattern: String): String
                 SimpleDateFormat(pattern)
             }
             .format(this)
+}
+
+/**
+ * 获得一天的开始时间: 0:00:00
+ * @return
+ */
+public fun Date.startTime(): Date{
+    val calendar = GregorianCalendar();
+    calendar.setTime(this);
+    calendar.set(Calendar.HOUR,0)
+    calendar.set(Calendar.MINUTE,0)
+    calendar.set(Calendar.SECOND,0)
+    calendar.set(Calendar.MILLISECOND,0)
+    System.out.println("开始时间：" + calendar.time)
+    return calendar.time
+}
+
+/**
+ * 获得一天的结束时间: 23:59:59
+ * @return
+ */
+public fun Date.endTime(): Date {
+    val calendar = GregorianCalendar();
+    calendar.setTime(this);
+    calendar.set(Calendar.HOUR,23)
+    calendar.set(Calendar.MINUTE,59)
+    calendar.set(Calendar.SECOND,59)
+    calendar.set(Calendar.MILLISECOND,999)
+    System.out.println("结束时间：" + calendar.time)
+    return calendar.time
 }
