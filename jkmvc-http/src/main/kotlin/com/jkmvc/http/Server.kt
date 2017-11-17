1cd1f93b33a41370aed9416d59fc9c16dc954df0
shijianhang <772910474.com>
2017-11-17 16:06:07 +0800

    完善http的日志

--- jkmvc-http/src/main/kotlin/com/jkmvc/http/Server.kt
+++ jkmvc-http/src/main/kotlin/com/jkmvc/http/Server.kt
@@ -1,7 +1,6 @@
 package com.jkmvc.http
 
 import com.jkmvc.common.Config
-import com.jkmvc.common.toQueryString
 import com.jkmvc.common.ucFirst
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
@@ -37,17 +36,12 @@ object Server:IServer {
         //　构建请求对象
         val req:Request = Request(request);
         if(debug){
+            // 路由
+            httpLogger.debug("请求uri: ${req.method} ${req.routeUri}")
 
-            if(req.isUpload()) // 上传请求，要等到设置了上传子目录，才能访问请求参数
-                httpLogger.debug("Upload 请求: ${req.routeUri}")
-            else{
-                if(req.isGet()) // get请求
-                    httpLogger.debug("${req.method} 请求: ${req.routeUri}?${req.queryString}")
-                else // post请求
-                    httpLogger.debug("${req.method} 请求: ${req.routeUri}?" + req.parameterMap.toQueryString())
-                // curl命令
+            // curl命令
+            if(!req.isUpload()) // 上传请求，要等到设置了上传子目录，才能访问请求参数
                 httpLogger.debug(req.buildCurlCommand())
-            }
         }
 
         //　如果是静态文件请求，则跳过路由解析
