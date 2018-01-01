9cf0bfd6dacbb717a6415241f676b8f698bffcbd
shijianhang <3076679995@qq.com>
2018-01-01 12:51:09 +0800

    抽取future的工具类

---
+++ jkmvc-common/src/main/kotlin/com/jkmvc/future/BasicFuture.kt
@@ -0,0 +1,187 @@
+package com.jkmvc.future
+
+import org.apache.http.concurrent.Cancellable
+import org.apache.http.concurrent.FutureCallback
+import java.util.concurrent.*
+
+/**
+ * 异步结果的通用实现
+ *   可以通过以下方法来表示完成状态: cancel() / failed() / completed()
+ *   参考包 org.apache.httpcomponents:httpcore:4.4.7 中的类 org.apache.http.concurrent.BasicFuture 的实现，但由于 BasicFuture 中的属性都是public的，所以子类无法实现IResponse接口，因此无法继承，只是复制
+ *
+ * @ClassName: BasicFuture
+ * @Description:
+ * @author shijianhang<772910474@qq.com>
+ * @date 2017-12-30 6:43 PM
+ */
+open class BasicFuture<T>(protected val callback: FutureCallback<T?>? = null): Future<T?>, Cancellable {
+
+    protected val lock = java.lang.Object()
+
+    /**
+     * 是否已完成
+     */
+    @Volatile protected var completed: Boolean = false
+
+    /**
+     * 是否被取消
+     */
+    @Volatile protected var cancelled: Boolean = false
+
+    /**
+     * 结果
+     */
+    @Volatile public var result: T? = null
+        protected set
+
+    /**
+     * 异常
+     */
+    @Volatile public var ex: Exception? = null
+        protected set
+
+    /**
+     * 是否被取消
+     * @return
+     */
+    public override fun isCancelled(): Boolean {
+        return this.cancelled
+    }
+
+    /**
+     * 是否已完成
+     * @return
+     */
+    public override fun isDone(): Boolean {
+        return this.completed
+    }
+
+    /**
+     * 尝试获得结果，如果该响应未完成，则抛出异常
+     * @return
+     */
+    @Throws(ExecutionException::class)
+    protected fun tryGetResult(): T? {
+        if (ex != null)
+            throw ExecutionException(ex)
+
+        if (cancelled)
+            throw CancellationException()
+
+        return this.result
+    }
+
+    /**
+     * 同步获得结果，无超时
+     * @return
+     */
+    @Synchronized @Throws(InterruptedException::class, ExecutionException::class)
+    public override fun get(): T? {
+        while (!this.completed)
+            lock.wait()
+
+        return tryGetResult()
+    }
+
+    /**
+     * 同步获得结果，有超时
+     *
+     * @param timeout
+     * @param unit
+     * @return
+     */
+    @Synchronized @Throws(InterruptedException::class, ExecutionException::class, TimeoutException::class)
+    public override fun get(timeout: Long, unit: TimeUnit): T? {
+        val msecs = unit.toMillis(timeout)
+        val startTime = if (msecs <= 0) 0 else System.currentTimeMillis()
+        var waitTime = msecs
+        if (this.completed)
+            return tryGetResult()
+
+        if (waitTime <= 0)
+            throw TimeoutException()
+
+        while (true) {
+            lock.wait(waitTime)
+            if (this.completed)
+                return tryGetResult()
+
+            waitTime = msecs - (System.currentTimeMillis() - startTime)
+            if (waitTime <= 0)
+                throw TimeoutException()
+        }
+    }
+
+    /**
+     * 完成
+     *
+     * @param result
+     * @return
+     */
+    public fun completed(result: T?): Boolean {
+        synchronized(this) {
+            if (this.completed) // 处理重入
+                return false
+
+            // 标识完成 + 记录结果
+            completed = true
+            this.result = result
+            lock.notifyAll()
+        }
+        // 回调
+        callback?.completed(result)
+        return true
+    }
+
+    /**
+     * 失败
+     *
+     * @param exception
+     * @return
+     */
+    public fun failed(exception: Exception): Boolean {
+        synchronized(this) {
+            if (completed) // 处理重入
+                return false
+
+            // 标识完成 + 记录异常
+            completed = true
+            ex = exception
+            lock.notifyAll()
+        }
+        // 回调
+        callback?.failed(exception)
+        return true
+    }
+
+    /**
+     * 取消
+     *
+     * @param mayInterruptIfRunning
+     * @return
+     */
+    public override fun cancel(mayInterruptIfRunning: Boolean): Boolean {
+        synchronized(this) {
+            if (this.completed) // 处理重入
+                return false
+
+            // 标识完成 + 标识取消
+            completed = true
+            cancelled = true
+            lock.notifyAll()
+        }
+        // 回调
+        callback?.cancelled()
+        return true
+    }
+
+    /**
+     * 取消
+     *
+     * @return
+     */
+    public override fun cancel(): Boolean {
+        return cancel(true)
+    }
+
+}
---
+++ jkmvc-common/src/main/kotlin/com/jkmvc/future/CompletedFuture.kt
@@ -0,0 +1,104 @@
+package com.jkmvc.future
+
+/**
+ * 已完成的异步结果，没有延后
+ *
+ * @ClassName: CompletedFuture
+ * @Description:
+ * @author shijianhang<772910474@qq.com>
+ * @date 2017-12-30 6:43 PM
+ */
+open class CompletedFuture<T>(protected val result: T): java.util.concurrent.Future<T> {
+    /**
+     * Returns `true` if this task completed.
+
+     * Completion may be due to normal termination, an cause, or
+     * cancellation -- in all of these cases, this method will return
+     * `true`.
+
+     * @return `true` if this task completed
+     */
+    public override fun isDone(): Boolean {
+        return true
+    }
+
+    /**
+     * Attempts to cancel execution of this task.  This attempt will
+     * fail if the task has already completed, has already been cancelled,
+     * or could not be cancelled for some other reason. If successful,
+     * and this task has not started when `cancel` is called,
+     * this task should never run.  If the task has already started,
+     * then the `mayInterruptIfRunning` parameter determines
+     * whether the thread executing this task should be interrupted in
+     * an attempt to stop the task.
+
+     *
+     * After this method returns, subsequent calls to [.isDone] will
+     * always return `true`.  Subsequent calls to [.isCancelled]
+     * will always return `true` if this method returned `true`.
+
+     * @param mayInterruptIfRunning `true` if the thread executing this
+     * * task should be interrupted; otherwise, in-progress tasks are allowed
+     * * to complete
+     * *
+     * @return `false` if the task could not be cancelled,
+     * * typically because it has already completed normally;
+     * * `true` otherwise
+     */
+    public override fun cancel(mayInterruptIfRunning: Boolean): Boolean {
+        return false
+    }
+
+    /**
+     * Returns `true` if this task was cancelled before it completed
+     * normally.
+
+     * @return `true` if this task was cancelled before it completed
+     */
+    public override fun isCancelled(): Boolean {
+        return false
+    }
+
+    /**
+     * Waits if necessary for at most the given time for the computation
+     * to complete, and then retrieves its result, if available.
+
+     * @param timeout the maximum time to wait
+     * *
+     * @param unit the time unit of the timeout argument
+     * *
+     * @return the computed result
+     * *
+     * @throws CancellationException if the computation was cancelled
+     * *
+     * @throws ExecutionException if the computation threw an
+     * * cause
+     * *
+     * @throws InterruptedException if the current thread was interrupted
+     * * while waiting
+     * *
+     * @throws TimeoutException if the wait timed out
+     */
+    public override fun get(timeout: Long, unit: java.util.concurrent.TimeUnit?): T {
+        return result
+    }
+
+    /**
+     * Waits if necessary for the computation to complete, and then
+     * retrieves its result.
+
+     * @return the computed result
+     * *
+     * @throws CancellationException if the computation was cancelled
+     * *
+     * @throws ExecutionException if the computation threw an
+     * * cause
+     * *
+     * @throws InterruptedException if the current thread was interrupted
+     * * while waiting
+     */
+    public override fun get(): T {
+        return result
+    }
+
+}
