5d7898ffaf177445df3060960ff4cf90a6dc9703
shijianhang <772910474.com>
2018-02-07 10:49:59 +0800

    优化代码

--- jkmvc-orm/src/main/kotlin/com/jkmvc/db/Db.kt
+++ jkmvc-orm/src/main/kotlin/com/jkmvc/db/Db.kt
@@ -77,6 +77,18 @@ class Db(protected val conn: Connection /* 数据库连接 */, public val name:S
         }
 
         /**
+         * 在操作之后关闭db
+         * @param statement db操作过程
+         */
+        public fun <T> closeAfter(statement: () -> Unit):Unit{
+            try{
+                statement()
+            }finally{
+                close()
+            }
+        }
+
+        /**
          * 转化为Long
          *
          * @param value
