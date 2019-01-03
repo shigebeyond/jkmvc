cca814ce41b965292ce21e34379f2061231af21e
shijianhang <772910474.com>
2019-01-03 15:24:45 +0800

    测试的oracle表支持自增id

--- jkmvc-orm/src/test/resources/test.oracle.sql
+++ jkmvc-orm/src/test/resources/test.oracle.sql
@@ -1,4 +1,4 @@
--- 用户表
+-- ------------------------  用户表 ------------------------ 
 CREATE TABLE "USER" (
     "ID" NUMBER(10) NOT NULL,
     "name" VARCHAR2(50 BYTE) NULL ,
@@ -9,7 +9,23 @@ CREATE TABLE "USER" (
 ALTER TABLE "USER" ADD CHECK ("ID" IS NOT NULL);
 ALTER TABLE "USER" ADD PRIMARY KEY ("ID");
 
--- 地址表
+-- Oracel使用 Sequence和Trigger来实现自增id
+CREATE SEQUENCE USER_ID_SEQ 
+  INCREMENT BY 1 -- 每次加1
+  START WITH 1 -- 从1开始
+  NOMAXVALUE -- 不设最大值
+  NOCYCLE -- 一直累加，不循环
+  NOCACHE -- 不建缓冲区
+  ;
+-- 定义Trigger
+CREATE OR REPLACE TRIGGER USER_ID_TRIG BEFORE
+INSERT ON USER FOR EACH ROW WHEN (new.ID is NULL)
+BEGIN
+  SELECT USER_ID_SEQ.NEXTVAL INTO:new.ID FROM dual;
+END;
+/ 
+
+-- ------------------------ 地址表 ------------------------ 
 CREATE TABLE "ADDRESS" (
     "ID" NUMBER(10) NOT NULL,
     "USER_ID" NUMBER(10) NOT NULL,
@@ -19,3 +35,19 @@ CREATE TABLE "ADDRESS" (
 
 ALTER TABLE "ADDRESS" ADD CHECK ("ID" IS NOT NULL);
 ALTER TABLE "ADDRESS" ADD PRIMARY KEY ("ID");
+
+-- Oracel使用 Sequence和Trigger来实现自增id
+CREATE SEQUENCE ADDRESS_ID_SEQ 
+  INCREMENT BY 1 -- 每次加1
+  START WITH 1 -- 从1开始
+  NOMAXVALUE -- 不设最大值
+  NOCYCLE -- 一直累加，不循环
+  NOCACHE -- 不建缓冲区
+  ;
+-- 定义Trigger
+CREATE OR REPLACE TRIGGER ADDRESS_ID_TRIG BEFORE
+INSERT ON ADDRESS FOR EACH ROW WHEN (new.ID is NULL)
+BEGIN
+  SELECT ADDRESS_ID_SEQ.NEXTVAL INTO:new.ID FROM dual;
+END;
+/ 
