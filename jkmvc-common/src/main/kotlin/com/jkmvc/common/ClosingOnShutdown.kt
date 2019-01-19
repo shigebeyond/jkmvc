d9fd9ce39b4c87f4b39953cfb806bf0e19545ea0
shijianhang <772910474.com>
2019-01-19 14:46:32 +0800

    抽取类 ClosingOnShutdown, 表示关机时要关闭的资源

---
+++ jkmvc-common/src/main/kotlin/com/jkmvc/common/ClosingOnShutdown.kt
@@ -0,0 +1,38 @@
+package com.jkmvc.common
+
+import java.io.Closeable
+import java.util.*
+
+/**
+ * 关机时要关闭的资源
+ *
+ * @Description:
+ * @author shijianhang<772910474@qq.com>
+ * @date 2017-12-16 3:48 PM
+ */
+abstract class ClosingOnShutdown : Closeable {
+
+    companion object {
+
+        /**
+         * 要关闭的资源
+         */
+        protected val closings: MutableList<Closeable> = Vector()
+
+        init {
+            // 添加关机事件钩子
+            Runtime.getRuntime().addShutdownHook(object : Thread() {
+                public override fun run() {
+                    println("--- Shutdown --- ")
+                    // 关闭资源
+                    for (c in closings)
+                        c.close()
+                }
+            })
+        }
+    }
+
+    init {
+        closings.add(this)
+    }
+}
