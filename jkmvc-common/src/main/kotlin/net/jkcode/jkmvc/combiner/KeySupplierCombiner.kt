2295e38402662115c6ff8d0655da848d5b7be088
shijianhang <772910474.com>
2019-04-15 19:06:22 +0800

    优化测试

--- jkmvc-common/src/main/kotlin/net/jkcode/jkmvc/combiner/KeySupplierCombiner.kt
+++ jkmvc-common/src/main/kotlin/net/jkcode/jkmvc/combiner/KeySupplierCombiner.kt
@@ -1,5 +1,6 @@
-package net.jkcode.jkmvc.common
+package net.jkcode.jkmvc.combiner
 
+import net.jkcode.jkmvc.common.isSuperClass
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.atomic.AtomicInteger
@@ -7,7 +8,7 @@ import java.util.concurrent.atomic.AtomicInteger
 /**
  * 结果的持有者
  */
-class ResultHolder<T>{
+class ResultHolder{
 
     /**
      * 等待数
@@ -17,17 +18,17 @@ class ResultHolder<T>{
     /**
      * 结果值
      */
-    protected var resultFuture: CompletableFuture<T>? = null
+    protected var resultFuture: CompletableFuture<Any>? = null
 
     /**
      * 异步调用取值操作, 但只调用一次
      *    只有第一个线程才能异步调用, 并缓存唯一的异步结果, 叫源异步结果
      *    所有线程均使用源异步结果, 但由于每个线程有自己的副作用(如在消费完后删除源异步结果), 因此每个线程只是使用源异步结果的克隆
      *
-     * @param supplier 取值操作
+     * @param supplier 取值操作, 如果返回值类型是 CompletableFuture, 则直接调用作为源异步结果
      * @return
      */
-    public fun supplyAsyncOnce(supplier: () -> T): CompletableFuture<T>{
+    public inline fun <reified T> supplyAsyncOnce(noinline supplier: () -> T): CompletableFuture<T>{
         // 增加等待数, 防止结果被删除
         if (waitNum.getAndIncrement() > 0 && resultFuture != null)
             return cloneThenDeleteSrcFuture()
@@ -35,8 +36,12 @@ class ResultHolder<T>{
         // 加锁来异步调用取值操作, 并缓存异步结果
         return synchronized(this) {
             // 第一个线程才异步调用取值操作
-            if (resultFuture == null)
-                resultFuture = CompletableFuture.supplyAsync(supplier)
+            if (resultFuture == null) {
+                if (CompletableFuture::class.java.isSuperClass(T::class.java)) // 如果异步取值操作, 主要是针对rpc的取值操作, 因为rpc调用是异步的, 调用完不是返回结果值, 而是返回future
+                    resultFuture = supplier.invoke() as CompletableFuture<Any>
+                else
+                    resultFuture = CompletableFuture.supplyAsync(supplier)
+            }
             // 其他线程直接使用异步结果
             cloneThenDeleteSrcFuture()
         }
@@ -46,12 +51,12 @@ class ResultHolder<T>{
      * 克隆源异步结果, 在消费完后删除
      * @return
      */
-    protected fun cloneThenDeleteSrcFuture(): CompletableFuture<T> {
+    protected fun <T> cloneThenDeleteSrcFuture(): CompletableFuture<T> {
         return resultFuture!!.thenApply {
             // 减少等待数, 同时在等待数为0(所有线程消费完)后, 删除异步结果, 以便下一个轮回
             if (waitNum.decrementAndGet() == 0)
                 resultFuture = null
-            it
+            it as T
         }
     }
 }
@@ -64,21 +69,21 @@ class ResultHolder<T>{
  * @author shijianhang<772910474@qq.com>
  * @date 2019-04-10 9:47 AM
  */
-class KeySupplierCombiner<T> {
+class KeySupplierCombiner {
 
     /**
      * <键 to 结果>
      */
-    protected val resultHolders: ConcurrentHashMap<Any, ResultHolder<T>> = ConcurrentHashMap()
+    protected val resultHolders: ConcurrentHashMap<Any, ResultHolder> = ConcurrentHashMap()
 
     /**
      * 针对当前key合并取值操作, 同步
      *
      * @param key
-     * @param supplier 取值操作
+     * @param supplier 取值操作, 如果返回值类型是 CompletableFuture, 则直接调用作为源异步结果
      * @return
      */
-    public fun combine(key: Any, supplier: () -> T): T {
+    public inline fun <reified T> combine(key: Any, noinline supplier: () -> T): T {
         return combineAsync(key, supplier).get()
     }
 
@@ -86,10 +91,10 @@ class KeySupplierCombiner<T> {
      * 针对当前key合并取值操作, 异步
      *
      * @param key
-     * @param supplier 取值操作
+     * @param supplier 取值操作, 如果返回值类型是 CompletableFuture, 则直接调用作为源异步结果
      * @return
      */
-    public fun combineAsync(key: Any, supplier: () -> T): CompletableFuture<T> {
+    public inline fun <reified T> combineAsync(key: Any, noinline supplier: () -> T): CompletableFuture<T> {
         val holder = resultHolders.getOrPut(key){
             ResultHolder()
         }
--- jkmvc-common/src/test/kotlin/net/jkcode/jkmvc/tests/KeySupplierCombinerTest.kt
+++ jkmvc-common/src/test/kotlin/net/jkcode/jkmvc/tests/KeySupplierCombinerTest.kt
@@ -1,15 +1,16 @@
 package net.jkcode.jkmvc.tests
 
-import net.jkcode.jkmvc.common.KeySupplierCombiner
+import net.jkcode.jkmvc.combiner.KeySupplierCombiner
 import net.jkcode.jkmvc.common.randomInt
 import org.junit.Test
+import java.util.concurrent.CompletableFuture
 import java.util.concurrent.atomic.AtomicInteger
 
 class KeySupplierCombinerTest{
 
     @Test
     fun testCombine(){
-        val c = KeySupplierCombiner<Int>()
+        val c = KeySupplierCombiner()
         val key = "test"
         var i = AtomicInteger(0)
 
@@ -26,7 +27,7 @@ class KeySupplierCombinerTest{
 
     @Test
     fun testCombineAsync(){
-        val c = KeySupplierCombiner<Int>()
+        val c = KeySupplierCombiner()
         val key = "test"
         var i = AtomicInteger(0)
 
@@ -44,9 +45,39 @@ class KeySupplierCombinerTest{
         Thread.sleep(2000)
     }
 
+    @Test
+    fun testCombineFuture(){
+        val c = KeySupplierCombiner()
+        val key = "test"
+        var i = AtomicInteger(0)
+
+        val run: () -> Unit = {
+            val f = c.combineAsync(key, this::supplyIntFuture)
+            f.thenAccept {
+                println("第${i.getAndIncrement()}个调用者: " + it.get())
+            }
+
+        }
+        Thread(run).start()
+        Thread(run).start()
+        Thread(run).start()
+
+        Thread.sleep(2000)
+    }
+
+    /**
+     * 同步的supplier
+     */
     fun supplyInt(): Int {
         println("调用 supply, 只调用一次")
         Thread.sleep(100)
         return randomInt(100)
+    }
+
+    /**
+     * 异步的supplier
+     */
+    fun supplyIntFuture(): CompletableFuture<Int> {
+        return CompletableFuture.supplyAsync(this::supplyInt)
     }
 }
