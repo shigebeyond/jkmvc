5d7d6ac4a0cef6be3894c75b4baff769d3ef680e
shijianhang <772910474.com>
2019-04-17 16:18:36 +0800

    优化KeySupplierCombiner

--- jkmvc-common/src/main/kotlin/net/jkcode/jkmvc/combiner/KeySupplierCombiner.kt
+++ jkmvc-common/src/main/kotlin/net/jkcode/jkmvc/combiner/KeySupplierCombiner.kt
@@ -1,62 +1,6 @@
 package net.jkcode.jkmvc.combiner
 
-import net.jkcode.jkmvc.common.isSuperClass
-import java.util.concurrent.CompletableFuture
-import java.util.concurrent.ConcurrentHashMap
-import java.util.concurrent.atomic.AtomicInteger
-
-/**
- * 结果的持有者
- */
-class ResultHolder{
-
-    /**
-     * 等待数
-     */
-    protected val waitNum: AtomicInteger = AtomicInteger(0)
-
-    /**
-     * 结果值
-     */
-    protected var resultFuture: CompletableFuture<Any>? = null
-
-    /**
-     * 异步调用取值操作, 但只调用一次
-     *    只有第一个线程才能异步调用, 并缓存唯一的异步结果, 叫源异步结果
-     *    所有线程均使用源异步结果, 但由于每个线程有自己的副作用(如在消费完后删除源异步结果), 因此每个线程只是使用源异步结果的克隆
-     *
-     * @param supplier 取值操作, 如果返回值类型是 CompletableFuture, 则直接调用作为源异步结果
-     * @return
-     */
-    public inline fun <reified T> supplyAsyncOnce(noinline supplier: () -> CompletableFuture<T>): CompletableFuture<T>{
-        // 增加等待数, 防止结果被删除
-        if (waitNum.getAndIncrement() > 0 && resultFuture != null)
-            return cloneThenDeleteSrcFuture()
-
-        // 加锁来异步调用取值操作, 并缓存异步结果
-        return synchronized(this) {
-            // 第一个线程才异步调用取值操作
-            if (resultFuture == null) {
-                resultFuture = supplier.invoke() as CompletableFuture<Any>
-            }
-            // 其他线程直接使用异步结果
-            cloneThenDeleteSrcFuture()
-        }
-    }
-
-    /**
-     * 克隆源异步结果, 在消费完后删除
-     * @return
-     */
-    protected fun <T> cloneThenDeleteSrcFuture(): CompletableFuture<T> {
-        return resultFuture!!.thenApply {
-            // 减少等待数, 同时在等待数为0(所有线程消费完)后, 删除异步结果, 以便下一个轮回
-            if (waitNum.decrementAndGet() == 0)
-                resultFuture = null
-            it as T
-        }
-    }
-}
+import net.jkcode.jkmvc.common.toFutureSupplier
 
 /**
  * 针对每个key的取值操作合并, 也等同于取值操作去重
@@ -66,27 +10,7 @@ class ResultHolder{
  * @author shijianhang<772910474@qq.com>
  * @date 2019-04-10 9:47 AM
  */
-class KeySupplierCombiner<T>(public val supplier: () -> CompletableFuture<T>) {
+class KeySupplierCombiner<RequestArgumentType /* 请求参数类型 */, ResponseType /* 响应类型 */>(supplier: (RequestArgumentType) -> ResponseType)
+    : KeyFutureSupplierCombiner<RequestArgumentType, ResponseType>(toFutureSupplier(supplier)) {
 
-    // CompletableFuture.supplyAsync(supplier)
-    
-    /**
-     * <键 to 结果>
-     */
-    protected val resultHolders: ConcurrentHashMap<Any, ResultHolder> = ConcurrentHashMap()
-
-    /**
-     * 针对当前key合并取值操作, 异步
-     *
-     * @param key
-     * @param supplier 取值操作, 如果返回值类型是 CompletableFuture, 则直接调用作为源异步结果
-     * @return
-     */
-    public fun add(key: Any): CompletableFuture<T> {
-        val holder = resultHolders.getOrPut(key){
-            ResultHolder()
-        }
-
-        return holder.supplyAsyncOnce(supplier)
-    }
 }
