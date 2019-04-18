aa5878225215c1006e4142e311174118a451eec3
shijianhang <772910474.com>
2019-04-18 10:16:00 +0800

    删除ratelimiter

---
+++ jkmvc-common/src/main/kotlin/net/jkcode/jkmvc/guard/IInterceptor.kt
@@ -0,0 +1,17 @@
+package net.jkcode.jkmvc.guard
+
+/**
+ * 拦截器
+ * @author shijianhang<772910474@qq.com>
+ * @date 2019-03-01 11:39 AM
+ */
+interface Interceptor<T> {
+
+    /**
+     * 前置处理请求
+     * @param req
+     * @return
+     */
+    fun preHandleRequest(req: T): Boolean
+
+}
