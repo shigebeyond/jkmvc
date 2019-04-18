b788f383a1ab68735897c2f36a467851b9c8a820
shijianhang <772910474.com>
2019-04-18 10:16:20 +0800

    调整命名

--- jkmvc-common/src/main/kotlin/net/jkcode/jkmvc/combiner/KeyFutureSupplierCombiner.kt
+++ jkmvc-common/src/main/kotlin/net/jkcode/jkmvc/combiner/KeyFutureSupplierCombiner.kt
@@ -65,6 +65,11 @@ class FutureHolder<RequestArgumentType /* 请求参数类型 */, ResponseType /*
 }
 
 /**
+ * 针对每个key的无值操作合并
+ */
+typealias KeyFutureRunCombiner<RequestArgumentType> = KeyFutureSupplierCombiner<RequestArgumentType, Void>
+
+/**
  * 针对每个key的取值操作合并, 也等同于取值操作去重
  *    如请求合并/cache合并等
  *    实现就是改进 ConcurrentMap.getOrPutOnce(key, defaultValue), 补上在多个取值操作处理完后删除key, 以便下一个轮回
--- jkmvc-common/src/main/kotlin/net/jkcode/jkmvc/combiner/KeySupplierCombiner.kt
+++ jkmvc-common/src/main/kotlin/net/jkcode/jkmvc/combiner/KeySupplierCombiner.kt
@@ -3,6 +3,11 @@ package net.jkcode.jkmvc.combiner
 import net.jkcode.jkmvc.common.toFutureSupplier
 
 /**
+ * 针对每个key的无值操作合并
+ */
+typealias KeyRunCombiner<RequestArgumentType> = KeySupplierCombiner<RequestArgumentType, Void>
+
+/**
  * 针对每个key的取值操作合并, 也等同于取值操作去重
  *    如请求合并/cache合并等
  *    实现就是改进 ConcurrentMap.getOrPutOnce(key, defaultValue), 补上在多个取值操作处理完后删除key, 以便下一个轮回
