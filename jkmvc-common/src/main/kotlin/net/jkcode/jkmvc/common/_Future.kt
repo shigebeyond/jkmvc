e871015197d4c4dbcdd86258954db99037cfc392
shijianhang <772910474.com>
2019-04-18 20:59:10 +0800

    添加帮助方法

--- jkmvc-common/src/main/kotlin/net/jkcode/jkmvc/common/_Future.kt
+++ jkmvc-common/src/main/kotlin/net/jkcode/jkmvc/common/_Future.kt
@@ -1,6 +1,8 @@
 package net.jkcode.jkmvc.common
 
 import java.util.concurrent.CompletableFuture
+import java.util.function.BiConsumer
+import java.util.function.Supplier
 
 /**
  * 等待并输出异步结果
@@ -28,5 +30,29 @@ public fun <RequestArgumentType, ResponseType> toFutureSupplier(supplier: (Reque
         CompletableFuture.supplyAsync({
             supplier.invoke(arg)
         })
+    }
+}
+
+/**
+ * 对supplier包装try/catch/finally
+ *
+ * @param supplier 取值函数
+ * @param complete 完成后的回调函数, 接收2个参数: 1 结果值 2 异常
+ */
+public fun <T> trySupplier(supplier: Supplier<T>, complete: BiConsumer<T?, Throwable?>){
+    var result:T? = null
+    var rh: Throwable? = null
+    try{
+        // 调用取值函数
+        result = supplier.get()
+        // 异步结果
+        if(result is CompletableFuture<*>)
+            (result as CompletableFuture<T>).whenComplete(complete) // 完成后回调
+    }catch (r: Throwable){
+        rh = r
+    }finally {
+        // 同步结果
+        if(result !is CompletableFuture<*>)
+            complete.accept(result, rh) // 完成后回调
     }
 }
