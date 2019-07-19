9c542ccb8f62508ee39cc8638b770ceba7936289
shijianhang <772910474.com>
2019-07-19 09:28:49 +0800

    支持按模块来切换启用日志

--- jkmvc-common/src/main/kotlin/net/jkcode/jkmvc/common/ModuleLogSwitcher.kt
+++ jkmvc-common/src/main/kotlin/net/jkcode/jkmvc/common/ModuleLogSwitcher.kt
@@ -8,6 +8,11 @@ import java.lang.reflect.Proxy
 
 /**
  * 按模块来切换启用日志
+ *    本项目中有多个模块, 如rpc/job/tracer/mq等, 模块之间会相互依赖, 如mq依赖rpc
+ *    我在开发某个模块时, 只对该模块日志感兴趣, 对其他模块日志不感兴趣, 这就需要禁用该模块日志
+ *    但一个模块会配置有多个logger(如rpc有register/client/server等几个logger), 禁用该模块日志, 要禁用这多个logger, 很繁琐
+ *    => 直接在模块级别来控制是否启用日志
+ *
  * @author shijianhang<772910474@qq.com>
  * @date 2019-07-18 7:30 PM
  */
@@ -18,13 +23,13 @@ class ModuleLogSwitcher(protected val module: String /* 模块 */) {
         /**
          * 模块日志切换配置
          */
-        public val config: Config = Config.instance("module-log-switcher")
+        public val config: Config = Config.instance("module-log-switcher", "yaml")
     }
 
     /**
      * 是否启用
      */
-    protected val enabled: Boolean = config[module]!!
+    protected val enabled: Boolean = config.getBoolean(module, true)!!
 
     /**
      * 是否禁用
--- jkmvc-common/src/main/kotlin/net/jkcode/jkmvc/common/_Logger.kt
+++ jkmvc-common/src/main/kotlin/net/jkcode/jkmvc/common/_Logger.kt
@@ -3,4 +3,5 @@ package net.jkcode.jkmvc.common
 import org.slf4j.LoggerFactory
 
 // 公用的日志
+internal val switcher = ModuleLogSwitcher("common")
 val commonLogger = LoggerFactory.getLogger("net.jkcode.jksoa.common")
