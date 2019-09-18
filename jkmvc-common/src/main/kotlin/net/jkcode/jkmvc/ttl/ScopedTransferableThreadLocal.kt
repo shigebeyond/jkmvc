35e811b656d655fcacc280b4bb47cb8afd536369
shijianhang <772910474@qq.com>
2019-09-18 23:18:46 +0800

    完善ScopedTransferableThreadLocal的值超过作用域被删除

--- jkmvc-common/src/main/kotlin/net/jkcode/jkmvc/ttl/ScopedTransferableThreadLocal.kt
+++ jkmvc-common/src/main/kotlin/net/jkcode/jkmvc/ttl/ScopedTransferableThreadLocal.kt
@@ -23,6 +23,11 @@ typealias Local2Value = MutableMap<ScopedTransferableThreadLocal<*>, SttlValue>
  *
  *    4. 所有的get()/set()操作必须在作用域内执行, 也就是说必须先调用 beginScope()
  *
+ *    5. 值的变动 vs 超过作用域的引用
+ *       endScope() 可能随时随地调用, 也就是说 SttlValue 随时可能被删除, 但可能某个线程调用了 SttlInterceptor.intercept(回调), 但此时回调还没触发, 也就是旧的 ScopedTransferableThreadLocal 对象还未恢复, 等恢复后引用的 SttlValue 却应该被删掉, 因此添加 deleted 属性来做是否已删除的判断
+ *       可能同一个线程读两次get(), 读的值不一样, 第一次不为null, 第二次为null, 具体以其他线程修改的时间或endScope()调用的时间为准
+ *       不过我本意是将 endScope() 交给开发者来自行调用, 从而保证 endScope() 是最后调用的, 往后不会有后续引用了, 从而保证不会作用域逃逸
+ *
  * @author shijianhang<772910474@qq.com>
  * @date 2019-09-17 8:32 AM
  */
@@ -68,6 +73,8 @@ open class ScopedTransferableThreadLocal<T>(public val supplier: (()->T)? = null
         /**
          * 设置当前线程线的本地值映射
          *    本地值映射: <ThreadLocal, 值>
+         *    在传递/恢复本地值时使用
+         *    endScope() 可能随时随地调用, 也就是说 SttlValue 随时可能被删除, 但可能某个线程调用了 SttlInterceptor.intercept(回调), 但此时回调还没触发, 也就是旧的 ScopedTransferableThreadLocal 对象还未恢复, 等恢复后引用的 SttlValue 却应该被删掉, 因此添加 deleted 属性来做是否已删除的判断
          *
          * @param local2Value
          */
@@ -75,7 +82,10 @@ open class ScopedTransferableThreadLocal<T>(public val supplier: (()->T)? = null
             //return local2Values.set(local2Value)
             val map = local2Values.get()
             map.clear()
-            map.putAll(local2Value)
+            // map.putAll(local2Value)
+            for((local, value) in local2Value)
+                if(!value.deleted) // 未删除
+                    map[local] = value
         }
 
     }
@@ -99,12 +109,17 @@ open class ScopedTransferableThreadLocal<T>(public val supplier: (()->T)? = null
 
     /**
      * 获得值
+     *    endScope() 可能随时随地调用, 也就是说 SttlValue 随时可能被删除, 但可能某个线程调用了 SttlInterceptor.intercept(回调), 但此时回调还没触发, 也就是旧的 ScopedTransferableThreadLocal 对象还未恢复, 等恢复后引用的 SttlValue 却应该被删掉, 因此添加 deleted 属性来做是否已删除的判断
+     *    可能同一个线程读两次get(), 读的值不一样, 第一次不为null, 第二次为null, 具体以其他线程修改的时间或endScope()调用的时间为准
      * @param initOnNull 当值为null时初始化值
      * @return
      */
     public fun get(initOnNull: Boolean = true): T {
         val local2Value = local2Values.get()
         var value = local2Value.get(this)
+        // 已删除
+        if(value != null && value.deleted)
+            value = null
         // 当值为null时初始化值
         if(value == null && initOnNull){
             value = SttlValue(supplier?.invoke())
@@ -129,6 +144,7 @@ open class ScopedTransferableThreadLocal<T>(public val supplier: (()->T)? = null
     /**
      * 作用域结束
      *    删除值, 要删除所有被传递的线程的值
+     *    endScope() 可能随时随地调用, 也就是说 SttlValue 随时可能被删除, 但可能某个线程调用了 SttlInterceptor.intercept(回调), 但此时回调还没触发, 也就是旧的 ScopedTransferableThreadLocal 对象还未恢复, 等恢复后引用的 SttlValue 却应该被删掉, 因此添加 deleted 属性来做是否已删除的判断
      */
     public override fun doEndScope() {
         //println("endScope")
@@ -141,8 +157,13 @@ open class ScopedTransferableThreadLocal<T>(public val supplier: (()->T)? = null
         if(value == null)
             return
 
+        // 标记已删除
+        value.deleted = true
+
         // 遍历每个被传递线程来删除值
-        for(t in value.threads){
+        while(value.threads.isNotEmpty()){
+            // 删除线程
+            val t = value.threads.pop()
             // 获得该线程的值
             val local2Value = local2Values.get(t)
             if(local2Value == null)
@@ -153,8 +174,6 @@ open class ScopedTransferableThreadLocal<T>(public val supplier: (()->T)? = null
             if(value2 == value)
                 local2Value.remove(this)
         }
-        // 值也删除所有线程
-        value.clearThreads()
     }
 
 }
--- jkmvc-common/src/main/kotlin/net/jkcode/jkmvc/ttl/SttlValue.kt
+++ jkmvc-common/src/main/kotlin/net/jkcode/jkmvc/ttl/SttlValue.kt
@@ -4,6 +4,7 @@ import java.util.*
 
 /**
  * 值, 有作用域的可传递的 ThreadLocal 中的值
+ *    ScopedTransferableThreadLocal.endScope() 可能随时随地调用, 也就是说 SttlValue 随时可能被删除, 但可能某个线程调用了 SttlInterceptor.intercept(回调), 但此时回调还没触发, 也就是旧的 ScopedTransferableThreadLocal 对象还未恢复, 等恢复后引用的 SttlValue 却应该被删掉, 因此添加 deleted 属性来做是否已删除的判断
  *
  * @author shijianhang<772910474@qq.com>
  * @date 2019-09-17 8:52 AM
@@ -13,7 +14,13 @@ class SttlValue(public var value: Any? = null) {
     /**
      * 被传递的线程
      */
-    public val threads: MutableList<Thread> = LinkedList()
+    internal val threads: Queue<Thread> = BlockingQueue()
+
+    /**
+     * 是否已删除
+     */
+    @Volatile
+    public var deleted: Boolean = false
 
     init {
         // 添加创建的线程
@@ -24,8 +31,9 @@ class SttlValue(public var value: Any? = null) {
      * 添加被传递的线程
      */
     public fun addThread(t: Thread = Thread.currentThread()){
-        //println("addThread: " + t.name)
-        threads.add(t)
+        if(!deleted)
+            //println("addThread: " + t.name)
+            threads.add(t)
     }
 
     /**
