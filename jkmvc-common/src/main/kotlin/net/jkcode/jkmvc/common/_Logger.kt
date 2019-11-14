e2f9fd0acb3f287f9933c794152eb4ae2a98b38b
shijianhang <772910474@qq.com>
2019-11-14 23:36:57 +0800

    完善日志

--- jkmvc-common/src/main/kotlin/net/jkcode/jkmvc/common/_Logger.kt
+++ jkmvc-common/src/main/kotlin/net/jkcode/jkmvc/common/_Logger.kt
@@ -1,12 +1,10 @@
 package net.jkcode.jkmvc.common
 
-import org.slf4j.LoggerFactory
-
 internal val switcher = ModuleLogSwitcher("common")
 
 // 公用的日志
 val commonLogger = switcher.getLogger("net.jkcode")
 // db的日志
-val dbLogger = LoggerFactory.getLogger("net.jkcode.jkmvc.db")
+val dbLogger = switcher.getLogger("net.jkcode.jkmvc.db")
 // http的日志
 val httpLogger = switcher.getLogger("net.jkcode.jkmvc.http")
