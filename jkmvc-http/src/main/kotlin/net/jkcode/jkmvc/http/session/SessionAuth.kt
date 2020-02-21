c5948882e77cd513575d729090417c34646ea184
shijianhang <772910474@qq.com>
2020-02-21 11:47:03 +0800

    完善会话

--- jkmvc-http/src/main/kotlin/net/jkcode/jkmvc/http/session/SessionAuth.kt
+++ jkmvc-http/src/main/kotlin/net/jkcode/jkmvc/http/session/SessionAuth.kt
@@ -16,10 +16,10 @@ class SessionAuth : Auth() {
     /**
      * 获得当前会话
      *
-     * @param create 是否创建新的会话
+     * @param create 如果没有会话, 是否创建新的会话
      * @return
      */
-    private fun getSession(create:Boolean = false): HttpSession {
+    private fun getSession(create:Boolean = true): HttpSession? {
         return HttpRequest.current().getSession(create);
     }
 
@@ -29,7 +29,7 @@ class SessionAuth : Auth() {
      */
     public override fun getUser(): IAuthUserModel?{
         // 从session中读取登录用户
-        return getSession(false).getAttribute("user") as IAuthUserModel?
+        return getSession(false)?.getAttribute("user") as IAuthUserModel?
     }
 
     /**
@@ -38,14 +38,14 @@ class SessionAuth : Auth() {
      */
     protected override fun afterLogin(user: IAuthUserModel) {
         // 保存登录用户到session中
-        getSession(true).setAttribute("user", user);
+        getSession(true)!!.setAttribute("user", user);
     }
 
     /**
      * 注销登录
      */
     public override fun logout(){
-        getSession(false).invalidate();
+        getSession(false)?.invalidate();
     }
 
 }
