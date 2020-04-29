64e21edabfdfad2826fc96963ea14658132d34ed
shijianhang <772910474.com>
2020-04-29 15:40:46 +0800

    完善安装

--- install.sh
+++ install.sh
@@ -3,7 +3,11 @@
 sed -i 's/env=dev/env=pro/g' gradle.properties
 
 # 安装到本地库
-gradle install -x test
+#gradle install -x test
+gradle :jkmvc-orm:install -x test
+gradle :jkmvc-http:install -x test
+gradle :jkmvc-server-jetty:install -x test
+gradle :jkmvc-tag:install -x test
 
 # 恢复开发环境
 sed -i 's/env=pro/env=dev/g' gradle.properties
