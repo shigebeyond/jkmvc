78fe1810b67aa85b3004dd3dd187159f3aed945a
shijianhang <3076679995@qq.com>
2022-04-24 09:32:15 +0800

    完善jphp变量抽取

--- jkmvc-http/src/main/resources/jphp/callController.php
+++ jkmvc-http/src/main/resources/jphp/callController.php
@@ -3,55 +3,56 @@ namespace php\jkmvc\http;
 define('APPPATH', dirname(__FILE__)); // 应用目录
 
 // controller基类
-abstract class IController {
-	public $req; // 请求对象
-	public $res; // 响应对象
+if(!class_exists('php\jkmvc\http\IController', FALSE)) // 检查controller基类, 一般不需要, 但如果修改了该文件, 会重新加载就会导致重复创建类
+    abstract class IController {
+        public $req; // 请求对象
+        public $res; // 响应对象
 
-	public function __construct(HttpRequest $req, HttpResponse $res){
-		$this->req = $req;
-		$this->res = $res;
-	}
+        public function __construct(HttpRequest $req, HttpResponse $res){
+            $this->req = $req;
+            $this->res = $res;
+        }
 
-    /**
-	 * 渲染视图
-	 * @param $file
-	 * @param $data
-	 * @param $is_return 是否返回视图内容，否则直接响应输出
-	 * @return string
-	 */
-	public function view($file, $data = NULL, $is_return = FALSE)
-	{
-		// 释放变量
-		if($data)
-		    extract($data, EXTR_REFS | EXTR_SKIP);
+        /**
+         * 渲染视图
+         * @param $file
+         * @param $data
+         * @param $is_return 是否返回视图内容，否则直接响应输出
+         * @return string
+         */
+        public function view($file, $data = NULL, $is_return = FALSE)
+        {
+            // 释放变量
+            if($data)
+                extract($data);
 
-		// 开输出缓冲
-		ob_start();
+            // 开输出缓冲
+            ob_start();
 
-		// 找到视图
-		$file = APPPATH . '/views/' . $file . '.php';
-		if(!file_exists($file))  // 检查视图文件
-			throw new Exception("View not exists: $file");
+            // 找到视图
+            $file = APPPATH . '/views/' . $file . '.php';
+            if(!file_exists($file))  // 检查视图文件
+                throw new Exception("View not exists: $file");
 
-		try {
-			// 加载视图, 并输出
-			include $file;
+            try {
+                // 加载视图, 并输出
+                include $file;
 
-			// 获得输出缓存
-			$content = ob_get_contents();
-			if($is_return)
-			    return $content;
+                // 获得输出缓存
+                $content = ob_get_contents();
+                if($is_return)
+                    return $content;
 
-			// 响应输出
-			$this->res->write($content);
-		}
-		finally
-		{
-			// 结束输出缓存
-			ob_end_clean();
-		}
-	}
-}
+                // 响应输出
+                $this->res->write($content);
+            }
+            finally
+            {
+                // 结束输出缓存
+                ob_end_clean();
+            }
+        }
+    }
 
 // 引入controller文件
 $controller = ucfirst($req->controller());
