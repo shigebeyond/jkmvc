e42f1e33232cb856b15e931a5e378ae71e724bcd
shijianhang <3076679995@qq.com>
2022-04-29 16:43:24 +0800

    优化命名

--- jkmvc-http/src/main/kotlin/net/jkcode/jkmvc/http/jphp/PhpView.kt
+++ jkmvc-http/src/main/kotlin/net/jkcode/jkmvc/http/jphp/PhpView.kt
@@ -12,7 +12,7 @@ import net.jkcode.jphp.ext.JphpLauncher
  * @author shijianhang<772910474@qq.com>
  * @date 8/25/17 9:49 AM
  */
-class JphpView(req: HttpRequest /* 请求对象 */, res: HttpResponse /* 响应对象 */, file:String/* 视图文件 */, vm: MutableMap<String, Any?> /* 视图模型 */): View(req, res, file, vm) {
+class PhpView(req: HttpRequest /* 请求对象 */, res: HttpResponse /* 响应对象 */, file:String/* 视图文件 */, vm: MutableMap<String, Any?> /* 视图模型 */): View(req, res, file, vm) {
 
     /**
      * 渲染php模板
