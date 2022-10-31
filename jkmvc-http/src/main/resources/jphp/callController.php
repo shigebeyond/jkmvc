ba9bcc1876d04e08a169921a4031e5de329a8151
shijianhang <3076679995@qq.com>
2022-10-31 16:43:19 +0800

    完善调用controller的php

--- jkmvc-http/src/main/resources/jphp/callController.php
+++ jkmvc-http/src/main/resources/jphp/callController.php
@@ -1,5 +1,5 @@
 <?php
-namespace php\jkmvc\http;
+namespace php\jkmvc\http; // 定义 IController 类时需要
 define('APPPATH', dirname(__FILE__)); // 应用目录
 
 // TODO: 为了优化性能, 可适当减少判断的代码, 如判断文件/类/方法存不存在, 以便减少php代码, 压榨点性能; 这样最后包一层try, 然后转化下对用户友好的异常
@@ -41,7 +41,7 @@ if(!class_exists('php\jkmvc\http\IController', FALSE)) // 检查controller基类
             // 找到视图
             $file = APPPATH . '/views/' . $file . '.php';
             if(!file_exists($file))  // 检查视图文件
-                throw new Exception("View not exists: $file");
+                throw new \Exception("View not exists: $file");
 
             try {
                 // 加载视图, 并输出
@@ -67,16 +67,16 @@ if(!class_exists('php\jkmvc\http\IController', FALSE)) // 检查controller基类
 $controller = ucfirst($req->controller());
 $file = APPPATH . '/controller/' . $controller . '.php';
 if(!file_exists($file)) // 检查controller文件
-    throw new Exception("$404[Controller file not exists: $controller]");
+    throw new \Exception("$404[Controller file not exists: $controller]");
 
 include $file;
 
 if(!class_exists($controller, FALSE)) // 检查controller类
-    throw new Exception("$404[Controller class not exists: $controller]");
+    throw new \Exception("$404[Controller class not exists: $controller]");
 
 $action = $req->action();
 if (!method_exists($controller, $action)) // 检查action方法
-    throw new Exception("$404[Controller {$controller} has no method: {$action}()]");
+    throw new \Exception("$404[Controller {$controller} has no method: {$action}()]");
 
 // 3 实例化controller
 $controller = new $controller($req, $res);
